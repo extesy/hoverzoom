8daf482da25a2fabf6ec9a68850b0c71337b54a7
ElusiveFox <thewomanintheredblouse@gmail.com>
2015-01-12 11:03:51 +1000

    Updated support for fetlife and added support for the kink family of websites.

--- plugins/fetlife.js
+++ plugins/fetlife.js
@@ -8,6 +8,15 @@ hoverZoomPlugins.push( {
     prepareImgLinks: function(callback) {
         var res = [];
         hoverZoom.urlReplace(res, 'a img[src*="flpics"]', /_\d+\.jpg/, '_958.jpg');
+        hoverZoom.urlReplace(res, 'a.main_pic span.fake_img', /_\d+\.jpg/, '_958.jpg');
+        // $('a.main_pic span.fake_img').each(function() {
+            // var fake_img = $(this), 
+                // link = fake_img.parents('a:eq(0)'), 
+                // src = fake_img.css('background-image');
+            // src = src.replace("url(", "").replace(")", "").replace("_720.jpg", "_958.jpg");
+            // link.data('hoverZoomSrc', [src]); 
+            // res.push(link);
+        // });
         callback($(res));
     }
 });
---
+++ plugins/kink.js
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2014 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+
+hoverZoomPlugins.push( {
+    name: 'kink',
+    prepareImgLinks: function(callback) {
+        var res = [];
+        hoverZoom.urlReplace(res, 'a img[src*="imagedb"]', /\/i\/h\/\d+/, '/i/h/830');
+        callback($(res));
+    }
+});
