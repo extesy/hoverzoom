788526dfaeb4331626210b9c882ca15e3d1051b6
Oleg Anashkin <oleg.anashkin@gmail.com>
2015-03-30 20:39:12 -0700

    add dirty.ru plugin

---
+++ plugins/dirty.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2015 Oleg Anashkin <oleg.anashkin@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+  name:'dirty.ru',
+  version:'0.1',
+  prepareImgLinks:function (callback) {
+    var res = [];
+    $('span[data-src]').filter(function () {
+      return $(this).data('src').match(/\/[^:]+\.(?:jpe?g|gif|png|webm|mp4|svg|webp|bmp|ico|xbm)(?:[\?#].*)?$/i);
+    }).each(function () {
+      var _this = $(this), data = _this.data();
+      if (!data.hoverZoomSrc) {
+        data.hoverZoomSrc = [data.src];
+        res.push(_this);
+      }
+    });
+
+    if (res.length) {
+      callback($(res));
+    }
+  }
+});
