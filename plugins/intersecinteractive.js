2af465a59db4319f08f94a70621335130ad23012
ElusiveFox <thewomanintheredblouse@gmail.com>
2015-04-10 19:00:16 +1000

    Added support for new Intersec Interactive galleries.

---
+++ plugins/intersecinteractive.js
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2014 Romain Vallet <romain.vallet@gmail.com>
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+
+hoverZoomPlugins.push( {
+    name: 'intersecinteractive',
+    prepareImgLinks: function(callback) {
+        var res = [];
+        hoverZoom.urlReplace(res, 'a img[src*="images"]', /\/(\d+)\.jpg/, '/images/$1.jpg');
+        hoverZoom.urlReplace(res, 'a img[src*="images"]', /\/poster_thumbnail\.jpg/, '/poster.jpg');
+        callback($(res));
+    }
+});
