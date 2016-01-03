1d49c78b308f75e65d6a5886f1e2a0b5579da9c5
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-01-03 13:47:40 -0800

    Update slickdeals plugin

--- plugins/slickdeals.js
+++ plugins/slickdeals.js
@@ -7,11 +7,21 @@ hoverZoomPlugins.push({
     version:'0.1',
     prepareImgLinks:function (callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'img[src$=".thumb"]',
             [/\/\d+x\d+\//, '.thumb'],
             ['/', '.attach']
         );
+
+        $('body').on('mouseenter', 'img[src$=".thumb"]', function() {
+            var img = $(this);
+            var url = img.attr('src').replace(/\/\d+x\d+\//, '/').replace('.thumb', '.attach');
+            img.data().hoverZoomSrc = [url];
+            img.addClass('hoverZoomLink');
+            hoverZoom.displayPicFromElement(img);
+        });
+
         callback($(res));
     }
 });
