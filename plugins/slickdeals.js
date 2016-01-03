1eec86de9f02fb6c278be3ebd78fe0b4aa24fed5
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-01-03 12:55:57 -0800

    Add support for slickdeals.net

---
+++ plugins/slickdeals.js
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2016 Oleg Anashkin <oleg.anashkin@gmail.com
+// Licensed under the MIT license, read license.txt
+
+var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'slickdeals',
+    version:'0.1',
+    prepareImgLinks:function (callback) {
+        var res = [];
+        hoverZoom.urlReplace(res,
+            'img[src$=".thumb"]',
+            [/\/\d+x\d+\//, '.thumb'],
+            ['/', '.attach']
+        );
+        callback($(res));
+    }
+});
