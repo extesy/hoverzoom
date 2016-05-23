df83b2ac32f343ebbdbf357c2cb180f351408260
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-05-22 21:04:44 -0700

    Don't show gallery for a single image

--- js/hoverzoom.js
+++ js/hoverzoom.js
@@ -561,11 +561,8 @@ var hoverZoom = {
                 if (options.showCaptions && !options.ambilightEnabled && linkData.hoverZoomCaption) {
                     hzCaption = $('<div/>', {id:'hzCaption', text:linkData.hoverZoomCaption}).css(hzCaptionCss).appendTo(hz.hzImg);
                 }
-                if (linkData.hoverZoomGallerySrc) {
-                    var info = '';
-                    if (linkData.hoverZoomGallerySrc.length > 0) {
-                        info = (linkData.hoverZoomGalleryIndex + 1) + '/' + linkData.hoverZoomGallerySrc.length;
-                    }
+                if (linkData.hoverZoomGallerySrc && linkData.hoverZoomGallerySrc.length > 1) {
+                    var info = (linkData.hoverZoomGalleryIndex + 1) + '/' + linkData.hoverZoomGallerySrc.length;
                     hzGallery = $('<div/>', {id:'hzGallery', text:info}).css(hzGalleryInfoCss).appendTo(hz.hzImg);
                     if (linkData.hoverZoomGalleryIndex == 0 && linkData.hoverZoomGallerySrc.length > 1) {
                         preloadGalleryImage(1);
@@ -944,7 +941,7 @@ var hoverZoom = {
         function documentOnMouseWheel(event) {
             if (imgFullSize) {
                 var link = hz.currentLink, data = link.data();
-                if (data.hoverZoomGallerySrc) {
+                if (data.hoverZoomGallerySrc && data.hoverZoomGallerySrc.length !== 1) {
                     event.preventDefault();
                     if (event.originalEvent.wheelDeltaY > 0) {
                         rotateGalleryImg(-1);
