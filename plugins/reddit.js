28ef8720cffc4ea9608647c104bec30d916d34fc
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-05-28 17:45:00 -0700

    Add support for i.redd.it. Fixes #92.

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -3,38 +3,57 @@
 
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
-    name:'Reddit',
-    version:'0.2',
-    prepareImgLinks:function (callback) {
-        $('.hoverZoomLink').each(function () {
-            var _this = $(this);
-            if (options.filterNSFW && _this.parents('.over18').length) {
-                _this.removeClass('hoverZoomLink');
-            }
-            _this.data().hoverZoomCaption = _this.parent().find('a.title').text();
-        });
-        
-        $('.link a.thumbnail img').one('mouseover', function() {
-            var link = this.parentNode;
-            if (!link.classList.contains('hoverZoomLink')) {
-              hoverZoom.prepareFromDocument($(link), link.href, function(doc) {
-                  var meta = doc.querySelector('meta[property="og:image"][content]');
-                  if (meta && !link.classList.contains('hoverZoomLink')) {
-                      return meta.content;
-                  } else {
-                      return false;
-                  }
-              });
-            }
-        });
+  name: 'Reddit',
+  version: '0.2',
+  prepareImgLinks: function (callback) {
+    $('.hoverZoomLink').each(function () {
+      var _this = $(this);
+      if (options.filterNSFW && _this.parents('.over18').length) {
+        _this.removeClass('hoverZoomLink');
+      }
+      _this.data().hoverZoomCaption = _this.parent().find('a.title').text();
+    });
 
-        var res = [];
-        $('a[href*="//i.reddituploads.com/"]').each(function () {
-            var img = $(this);
-            img.data('hoverZoomSrc', [img.attr('href')]);
-            img.data('hoverZoomCaption', [img.text()]);
-            res.push(img);
+    $('.link a.thumbnail img').one('mouseover', function () {
+      var link = this.parentNode;
+      if (!link.classList.contains('hoverZoomLink')) {
+        hoverZoom.prepareFromDocument($(link), link.href, function (doc) {
+          var meta = doc.querySelector('meta[property="og:image"][content]');
+          if (meta && !link.classList.contains('hoverZoomLink')) {
+            return meta.content;
+          } else {
+            return false;
+          }
         });
-        callback($(res));
-    }
+      }
+    });
+
+    var res = [];
+
+    $('a[href*="//i.reddituploads.com/"]').each(function () {
+      var img = $(this);
+      img.data('hoverZoomSrc', [img.attr('href')]);
+      img.data('hoverZoomCaption', [img.text()]);
+      res.push(img);
+    });
+
+    $('div[data-cachedhtml*="//i.redd.it/"]').each(function () {
+      const div = $(this);
+      const html = $(div.attr('data-cachedhtml'));
+      const anchor = html.find('a[href*="//i.redd.it/"]');
+      if (anchor.length > 0) {
+        var link = anchor.attr('href');
+        var post = div.parent().parent();
+        var title = post.find('a.title').text();
+        post.select('a').each(function () {
+          var img = $(this);
+          img.data('hoverZoomSrc', [link]);
+          img.data('hoverZoomCaption', [title]);
+          res.push(img);
+        });
+      }
+    });
+
+    callback($(res));
+  }
 });
