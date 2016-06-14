77d46c22c58ac6b94189960b02a64fd060dc5b93
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-06-13 22:27:12 -0700

    Update localization

--- release.cmd
+++ release.cmd
@@ -1,3 +1,3 @@
 @echo off
 del hoverzoom.zip >nul
-winrar a -afzip -m5 -r hoverzoom.zip _locales\* css\* fonts\* html\* images\* js\* plugins\* LICENSE manifest.json
+winrar a -afzip -m5 -r hoverzoom.zip _locales\* css\* fonts\* html\* images\* js\* plugins\* LICENSE manifest.json -xnode_modules
