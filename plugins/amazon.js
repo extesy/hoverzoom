afeaa6ff0d270327affa3bb9548952c1113223f9
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-06-16 22:38:20 -0700

    Fix amazon

--- plugins/amazon.js
+++ plugins/amazon.js
@@ -4,7 +4,7 @@ hoverZoomPlugins.push({
     prepareImgLinks:function (callback) {
         var res = [];
         hoverZoom.urlReplace(res,
-            'img[src*=".images-amazon.com"]:not([src*="g-ecx.images-amazon.com"]), img[src*="/img.amazon."], .iv_thumb_image, img[src*="/ciu/"]',
+            'img[src*=".images-amazon.com"]:not([src*="g-ecx.images-amazon.com"]), img[src*=".ssl-images-amazon.com"]:not([src*="g-ecx.ssl-images-amazon.com"]), mg[src*="/img.amazon."], .iv_thumb_image, img[src*="/ciu/"]',
             /_.+_\./,
             ''
         );
