3ee2639aaa9244e80943763703a3ffa77bc8b82a
Oleg Anashkin <oleg.anashkin@gmail.com>
2016-11-14 14:25:37 -0800

    Update n11.js
    Code style update

--- plugins/n11.js
+++ plugins/n11.js
@@ -6,10 +6,8 @@ hoverZoomPlugins.push({
         $('img[src*=".n11.com.tr/a1/"], img[data-original*=".n11.com.tr/a1/"]').each(function () {
             var img = $(this);
             var src = img.attr('data-original') || img.attr('src');
-            if (!src) {
-				return;
-			}
-			src = src.replace(/\.com\.tr\/a1\/\d+/, options.showHighRes ? '.com.tr/a1/org' : '.com.tr/a1/1024');
+            if (!src) return;
+            src = src.replace(/\.com\.tr\/a1\/\d+/, options.showHighRes ? '.com.tr/a1/org' : '.com.tr/a1/1024');
             img.data().hoverZoomSrc = [src];
             res.push(img);
         });
