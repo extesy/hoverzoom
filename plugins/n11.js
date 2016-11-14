622c758b9fda9dd09d0968e15a2b2395100309a4
MhdHejazi <mohammadhi@gmail.com>
2016-11-14 20:25:20 +0300

    Enable HoverZoom for unloaded images on n11.com.

--- plugins/n11.js
+++ plugins/n11.js
@@ -3,11 +3,16 @@ hoverZoomPlugins.push({
     name:'n11',
     prepareImgLinks:function (callback) {
         var res = [];
-        hoverZoom.urlReplace(res,
-            'img[src*=".n11.com.tr/a1/"]',
-            /\.com\.tr\/a1\/\d+/,
-            options.showHighRes ? '.com.tr/a1/org' : '.com.tr/a1/1024'
-        );
+        $('img[src*=".n11.com.tr/a1/"], img[data-original*=".n11.com.tr/a1/"]').each(function () {
+            var img = $(this);
+            var src = img.attr('data-original') || img.attr('src');
+            if (!src) {
+				return;
+			}
+			src = src.replace(/\.com\.tr\/a1\/\d+/, options.showHighRes ? '.com.tr/a1/org' : '.com.tr/a1/1024');
+            img.data().hoverZoomSrc = [src];
+            res.push(img);
+        });
         callback($(res));
     }
 });
