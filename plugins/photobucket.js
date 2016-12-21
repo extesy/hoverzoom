8d7d2462321f468815a4e5f30495c863ccd16987
GrosPoulet <grosnaingrincheux@gmail.com>
2016-12-21 10:11:50 +0100

    Fix for plugin: photobucket.js

--- plugins/photobucket.js
+++ plugins/photobucket.js
@@ -1,7 +1,7 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Photobucket',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
         hoverZoom.urlReplace(res,
@@ -9,6 +9,15 @@ hoverZoomPlugins.push({
             ['//th', '/th_'],
             ['//i', '/'],
             ':first'
+        );
+        
+        //ex:
+        //http://rs124.pbsrc.com/albums/p9/Timaestro2/500x_spheremain.jpg~c100  <--- small image
+        //http://i124.photobucket.com/albums/p9/Timaestro2/500x_spheremain.jpg  <--- large image
+        hoverZoom.urlReplace(res,
+            'img[src*="pbsrc"]',
+            [/\/\/([a-z]+)/, 'pbsrc', /~.*/],
+            ['//i', 'photobucket', '']
         );
         callback($(res));
     }
