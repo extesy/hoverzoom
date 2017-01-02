10c36fd68fc719d5b67f9f02893bb044ddec6a7a
Dharak <vasavdadharak@gmail.com>
2017-01-02 13:17:09 -0800

    Changed files in /hoverzoom/js for Microsoft Edge
    Change some functions in the source JS files so it's compatible with Microsoft Edge

--- js/viewTab.js
+++ js/viewTab.js
@@ -1,4 +1,4 @@
-chrome.runtime.sendMessage({action:'getOptions'}, function (options) {
+browser.runtime.sendMessage({action:'getOptions'}, function (options) {
     window.addEventListener('keydown', function (event) {
         if (event.which == options.openImageInTabKey) {
             window.close();
