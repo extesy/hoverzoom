9be955c3a3c0ecbff7b5c0526bca29041fe1ab14
Oleg Anashkin <oleg.anashkin@gmail.com>
2017-03-05 16:58:56 -0800

    Hide captions while positioning image. Fixes #225.

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -5,7 +5,8 @@ hoverZoomPlugins.push({
         var res = [];
         $('img[src*="/e35/"]').each(function () {
             var img = $(this), link = img.parent().parent().parent();
-            link.data().hoverZoomSrc = [img.attr('src').replace('/s640x640/sh0.08/', '/')];
+            var url = img.attr('src').replace('/sh0.08/', '/').replace(/\/[sp]\d\d\dx\d\d\d\//, '/');
+            link.data().hoverZoomSrc = [url];
             res.push(link);
         });
         // hoverZoom.urlReplace(res, 'img[src*="/e35/"]', '/s640x640/sh0.08/', '/', 'a');
