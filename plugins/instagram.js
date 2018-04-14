9e41c8651cd061153613b84557475ce02b40c532
Oleg Anashkin <oleg.anashkin@gmail.com>
2018-04-13 20:06:00 -0700

    Improve instagram support. Fixes #358, fixes #335.

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -2,14 +2,11 @@ var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Instagram',
     prepareImgLinks:function (callback) {
-        var res = [];
-        $('img[src*="/e35/"]').each(function () {
-            var img = $(this), link = img.parent().parent().parent();
-            var url = img.attr('src').replace('/sh0.08/', '/').replace(/\/[sp]\d\d\dx\d\d\d\//, '/');
-            link.data().hoverZoomSrc = [url];
-            res.push(link);
+        $('body').on('mouseenter', 'a[href*="?taken-by"]', function () {
+            var link = $(this), img = link.find('img');
+            link.data().hoverZoomSrc = [img.attr('src')];
+            link.addClass('hoverZoomLink');
+            hoverZoom.displayPicFromElement(link);
         });
-        // hoverZoom.urlReplace(res, 'img[src*="/e35/"]', '/s640x640/sh0.08/', '/', 'a');
-        callback($(res));
     }
 });
