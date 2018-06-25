a5ddc77602b61db3ceda6670620ec71a233d204d
ivan2340 <9105175+ivan2340@users.noreply.github.com>
2018-06-25 19:27:34 +0200

    Instagram SSL Fix

--- plugins/instagram_a.js
+++ plugins/instagram_a.js
@@ -4,7 +4,7 @@ hoverZoomPlugins.push({
     prepareImgLinks:function (callback) {
         var res = [],
             search = /.*(?:instagr\.am|instagram\.com)\/p\/([^\/]+).*/i,
-            replace = 'http://instagr.am/p/$1/media/?size=l';
+            replace = 'https://instagr.am/p/$1/media/?size=l';
         $('a[href*="instagr.am/p/"], a[href*="instagram.com/p/"], a[data-expanded-url*="nstagr.am/p/"], a[data-expanded-url*="nstagram.com/p/"]').each(function () {
             var link = $(this), data = link.data(), url = this.dataset['expandedUrl'] || this.href;
             if (!data.hoverZoomSrc && !link.hasClass('compFrontside') && !link.hasClass('compFlipside')) {
