5df7db2194cdc457570ad1106d9adca4d553fcc1
Lukas Sorensen <lsorensen@sportsdigita.com>
2018-12-31 17:06:07 -0600

    fix disable current site feature in popup

--- js/popup.js
+++ js/popup.js
@@ -44,7 +44,8 @@ function askTabsPermissions() {
 }
 
 function setTabHook(options) {
-    chrome.tabs.query({active: true}, function (tab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabArr) {
+        var tab = tabArr[0];
         siteDomain = tab.url.split('/', 3)[2];
         $('#lblToggle').text(chrome.i18n.getMessage(options.whiteListMode ? 'popEnableForSite' : 'popDisableForSite', siteDomain));
         $('#chkExtensionDisabled')[0].checked = !options.extensionEnabled;
