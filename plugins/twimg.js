d4582923ae3663731d143b51fb55b508a1175cb8
mmakarwich10 <mist_shaker@yahoo.com>
2019-09-19 11:49:22 -0500

    #455: Fix for profile images on Twitter no longer loading
    - Added another urlReplace on twimg.js to account for the new mapping for the thumbnail and the full-size image.
    
    - Also turned off the pointer-events for the new <div> that is covering the profile thumbs, and is preventing hoverzoom from accessing the <img> element.

--- plugins/twimg.js
+++ plugins/twimg.js
@@ -1,7 +1,7 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Twimg.com',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
         hoverZoom.urlReplace(res,
@@ -9,6 +9,12 @@ hoverZoomPlugins.push({
             /_(normal|mini)/,
             ''
         );
+        hoverZoom.urlReplace(res,
+            'img[src*="twimg.com"]',
+            /_bigger/,
+            '_400x400'
+        );
+        $('div.css-1dbjc4n.r-1twgtwe.r-sdzlij.r-rs99b7.r-1p0dtai.r-1mi75qu.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af').css('pointer-events', 'none');
         callback($(res));
     }
 });
