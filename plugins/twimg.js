1ea6b09a972e0626140a62bbeb649c37dc279331
mmakarwich10 <mist_shaker@yahoo.com>
2019-09-19 13:37:51 -0500

    #455: Updated fix
    Updated selector for div covering profile thumb to make it more stable.

--- plugins/twimg.js
+++ plugins/twimg.js
@@ -14,7 +14,7 @@ hoverZoomPlugins.push({
             /_bigger/,
             '_400x400'
         );
-        $('div.css-1dbjc4n.r-1twgtwe.r-sdzlij.r-rs99b7.r-1p0dtai.r-1mi75qu.r-1d2f490.r-u8s1d.r-zchlnj.r-ipm5af').css('pointer-events', 'none');
+        $('a[aria-haspopup="false"] > div:nth-child(2)').css('pointer-events', 'none');
         callback($(res));
     }
 });
