c40b75c143ce5f47df32a3ee4d4234b206415b99
GrosPoulet <grosnaingrincheux@gmail.com>
2020-04-13 16:36:30 +0200

    Fix for Amazon plug-in
    Add support for : Amazon review images #495

--- plugins/amazon.js
+++ plugins/amazon.js
@@ -1,11 +1,12 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Amazon',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
         hoverZoom.urlReplace(res,
             'img[src*=".images-amazon.com"]:not([src*="g-ecx.images-amazon.com"]), img[src*=".ssl-images-amazon.com"]:not([src*="g-ecx.ssl-images-amazon.com"]), mg[src*="/img.amazon."], .iv_thumb_image, img[src*="/ciu/"]',
-            /_.+_\./,
+            /_.+\./,
             ''
         );
         callback($(res));
