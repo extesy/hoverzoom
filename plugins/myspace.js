fa61a24a795c522952a790b78f2029aed785ca13
GrosPoulet <grosnaingrincheux@gmail.com>
2020-04-24 18:07:55 +0200

    Fix for MySpace plug-in
    Fix for MySpace plug-in

--- plugins/myspace.js
+++ plugins/myspace.js
@@ -1,14 +1,24 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'MySpace',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'a img[src*="ac-images.myspacecdn.com"], a img[src*="images.socialplan.com"]',
             [/\/[sm]_/, '_t.'],
             ['/l_', '_p.']
         );
+
+        // /300x300.jpg -> /full.jpg
+        var reNxN = /\/\d+[xX]\d+\./;
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            reNxN,
+            '/full.'
+        );
+
         callback($(res));
     }
 });
