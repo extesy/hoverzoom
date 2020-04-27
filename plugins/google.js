f101894813e0d78fe5cc1bb2e02eb78e59cd8206
GrosPoulet <grosnaingrincheux@gmail.com>
2020-04-27 11:32:48 +0200

    Fix for Google plug-in
    Fix for Google images search.
    Mostly inspired from chocolatebot's userscript : Google DWIMages, but
    without the need of GreaseMonkey.

--- plugins/google.js
+++ plugins/google.js
@@ -1,6 +1,7 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Google',
+    version:'2.0',
     prepareImgLinks:function (callback) {
 
         // Google+ full page viewer
@@ -24,6 +25,45 @@ hoverZoomPlugins.push({
             /.*imgurl=([^&]+).*/,
             '$1'
         );
+
+        // Extract direct links to images using document.scripts data (no api call(s) needed)
+        // Mostly inspired from chocolatebot's userscript : Google DWIMages 
+        // You can find it here: https://greasyfork.org/fr/scripts/29420-google-dwimages
+        // Data structure:
+        // "data-tbnid",["thumbnail url",h,w],["image url we are looking for",h,w]
+        // for instance:
+        // "zEevaU3QYshNNM",["https://encrypted-tbn0.gstatic.com/images?q\u003dtbn%3AANd9GcS2TiJ5s8_9L8dtcfU8uhN7eBaJXUPU8Macpmkjs-HZDAOdiJtz\u0026usqp\u003dCAU",225,225],["https://www.savethedeco.com/12079-large_default/ballon-tete-de-singe-87-cm.jpg",800,800]
+        function parseScripts() {
+            scripts = Array.from(document.scripts);
+            callbacks = scripts.filter(script => /^AF_initDataCallback\b/.test(script.text));
+            dataFromScripts = callbacks.pop().text;
+        };
+
+        parseScripts();
+
+        // Loop through thumbnails and when possible add hrefs = images urls found in scripts
+        $('div[data-tbnid]').each(function() {
+
+           // tbnid = thumbnail id referenced in scripts'data
+           tbnid = '"' + this.dataset.tbnid + '",';
+
+           // lookup data extracted from scripts
+           if (dataFromScripts.indexOf(tbnid) == -1) { return; }
+
+           firstquoteIndex = dataFromScripts.indexOf('[', dataFromScripts.indexOf('[', dataFromScripts.indexOf(tbnid)) + 1) + 1;
+           lastquoteIndex = dataFromScripts.indexOf('"', firstquoteIndex + 1);
+           url = dataFromScripts.substring(firstquoteIndex + 1, lastquoteIndex);
+
+           if (url == undefined || url == "") { return; }
+
+           links = $(this).find('a');
+           if (links.length > 0) {
+               // update image link (1st link) with url
+               imageLink = links.eq(0);
+               imageLink.attr('href', url);
+           }
+        });
+
         callback($(res));
 
         // remove this when old google image is retired
