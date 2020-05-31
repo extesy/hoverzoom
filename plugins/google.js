7014786f1894ace8cc577a4ccbdac1d3ef30a233
GrosPoulet <grosnaingrincheux@gmail.com>
2020-05-31 19:28:38 +0200

    Rewrite of Google Images plug-in
    This new version of plug-in catches "on the fly" data sent by Google servers when user scrolls down images results.
    Note: This method could be applied to other plug-ins, removing the need for API calls.

--- plugins/google.js
+++ plugins/google.js
@@ -1,15 +1,16 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Google',
-    version:'2.0',
+    version:'3.0',
     prepareImgLinks:function (callback) {
+        var res = [];
+        var initData = null;
 
         // Google+ full page viewer
         if (location.search.indexOf('pid=') > -1) {
             return;
         }
 
-        var res = [];
         hoverZoom.urlReplace(res,
             'img[src*=".googleusercontent.com/"], img[src*=".ggpht.com/"]',
             /(\/|=)(w\d{2,}-h\d{2,}|[hws]\d{2,})(-[npckorw]+)*(\/|$)/,
@@ -26,35 +27,132 @@ hoverZoomPlugins.push({
             '$1'
         );
 
-        // Extract direct links to images using document.scripts data (no api call(s) needed)
-        // Mostly inspired from chocolatebot's userscript : Google DWIMages 
+        // Hook Google 'Open' XMLHttpRequests to catch data & metadata associated with pictures displayed
+        // These requests are issued by client to Google servers in order to obtain new data when user scrolls down
+        // Hooked data is stored in scripts in DOM with class='hoverZoomHookedData'
+        if ($('script.hoverZoomHook').length == 0) { // Inject hook script in document if not already there
+            var hookScript = document.createElement('script');
+            hookScript.type = 'text/javascript';
+            hookScript.text = `if (typeof oldXHROpen !== 'function') { // Hook only once!
+                console.log('Hooking XHROpen');
+                oldXHROpen = window.XMLHttpRequest.prototype.open;
+                window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+                    // catch responses
+                    this.addEventListener('load', function() {
+                        try {
+                            //console.log('load: ' + this.responseText);
+                            // store response as plain text in a <script> element for later usage by plug-in
+                            var hookedDataScript = document.createElement('script');
+                            hookedDataScript.type = 'text';
+                            hookedDataScript.text = this.responseText;
+                            hookedDataScript.classList.add('hoverZoomHookedData'); // add specific class for easier retrieval
+                            (document.head || document.documentElement).appendChild(hookedDataScript);
+                            // Add & remove empty <a> element to/from DOM to trigger HoverZoom,
+                            // so data & metadata just added to DOM in <script> element can be exploited
+                            var fakeA = document.createElement('a');
+                            (document.head || document.documentElement).appendChild(fakeA);
+                            (document.head || document.documentElement).removeChild(fakeA);
+                        } catch {}
+                    });
+                    // Proceed with original function
+                    return oldXHROpen.apply(this, arguments);
+                }
+            }`;
+            hookScript.classList.add('hoverZoomHook');
+            (document.head || document.documentElement).appendChild(hookScript);
+        }
+
+        function cleanUrl(url) {
+            if (url != undefined) {
+                url = url.replace(/\\\\/g, '\\');
+                url = url.replace(/\\[uU]00/g, '%');
+                url = decodeURIComponent(decodeURIComponent(url));
+            }
+            return url;
+        }
+
+        // Extract direct links for images:
+        // - extract the data for the first ≈ 100 images out of the <script> element embedded in the page
+        // - then use data received from server when user scrolls down (catched & stored in <script> elements appended to DOM)
+        // Mostly inspired from chocolatebot's userscript : Google DWIMages
         // You can find it here: https://greasyfork.org/fr/scripts/29420-google-dwimages
         // Data structure:
         // "data-tbnid",["thumbnail url",h,w],["image url we are looking for",h,w]
         // for instance:
         // "zEevaU3QYshNNM",["https://encrypted-tbn0.gstatic.com/images?q\u003dtbn%3AANd9GcS2TiJ5s8_9L8dtcfU8uhN7eBaJXUPU8Macpmkjs-HZDAOdiJtz\u0026usqp\u003dCAU",225,225],["https://www.savethedeco.com/12079-large_default/ballon-tete-de-singe-87-cm.jpg",800,800]
-        function parseScripts() {
-            scripts = Array.from(document.scripts);
-            callbacks = scripts.filter(script => /^AF_initDataCallback\b/.test(script.text));
-            dataFromScripts = callbacks.pop().text;
-        };
 
-        parseScripts();
+        // Extract data in initDataCallback
+        function extractInitData(tbnid) {
+            console.log('extractInitData');
+            let url = undefined;
+            if (initData == null) {
+                if (document.scripts == undefined) return url;
+                let scripts = Array.from(document.scripts);
+                let goodScripts = scripts.filter(script => /^AF_initDataCallback\b/.test(script.text));
+                if (goodScripts.length != 2) return url;
+                initData = goodScripts.pop().text;
+            }
+            let tbnidq = '"' + tbnid + '",';
+            if (initData.indexOf(tbnidq) != -1) {
+                let firstquoteIndex = initData.indexOf('[', initData.indexOf('[', initData.indexOf(tbnidq)) + 1) + 1;
+                let lastquoteIndex = initData.indexOf('"', firstquoteIndex + 1);
+                url = initData.substring(firstquoteIndex + 1, lastquoteIndex);
+            }
+            url = cleanUrl(url);
+            return url;
+        }
+
+        // Extract hooked data stored in <script> elements appended to DOM
+        function extractHookedData(tbnid) {
+            console.log('extractHookedData');
+            let url = undefined;
+            if (document.scripts == undefined) return url;
+            let scripts = Array.from($('script.hoverZoomHookedData'));
+            let goodScripts = scripts.filter(script => script.text.indexOf(tbnid) != -1);
+            if (goodScripts.length == 0) return url;
+            let dataFromScript = goodScripts[0].text;
+            dataFromScript = dataFromScript.replace(/\\n/g, '').replace(/\\"/g, '"');
+            let tbnidq = '"' + tbnid + '",';
+            if (dataFromScript.indexOf(tbnidq) != -1) {
+                let firstquoteIndex = dataFromScript.indexOf('[', dataFromScript.indexOf('[', dataFromScript.indexOf(tbnidq)) + 1) + 1;
+                let lastquoteIndex = dataFromScript.indexOf('"', firstquoteIndex + 1);
+                url = dataFromScript.substring(firstquoteIndex + 1, lastquoteIndex);
+            }
+            url = cleanUrl(url);
+            return url;
+        }
 
         // Loop through thumbnails and when possible add hrefs = images urls found in scripts
+        // tbnid = thumbnail id referenced in scripts'data
         $('div[data-tbnid]').each(function() {
 
-           // tbnid = thumbnail id referenced in scripts'data
-           tbnid = '"' + this.dataset.tbnid + '",';
+            if ($(this).find('a.hoverZoomLink').length > 0) return;
 
-           // lookup data extracted from scripts
-           if (dataFromScripts.indexOf(tbnid) == -1) { return; }
+            let tbnid = this.dataset.tbnid;
+            let hrefPage = undefined;
+            let url = undefined;
+            let imageLink = undefined;
 
-           firstquoteIndex = dataFromScripts.indexOf('[', dataFromScripts.indexOf('[', dataFromScripts.indexOf(tbnid)) + 1) + 1;
-           lastquoteIndex = dataFromScripts.indexOf('"', firstquoteIndex + 1);
-           url = dataFromScripts.substring(firstquoteIndex + 1, lastquoteIndex);
+            // check if url is already stored
+            url = sessionStorage.getItem('url_' + tbnid);
+            if (url != undefined) {
+                console.log('photo fullsizeUrl (from sessionStorage):' + url);
+            }
 
-           if (url == undefined || url == "") { return; }
+            // if not stored then lookup data extracted from scripts
+            if (url == undefined) {
+                url = extractInitData(tbnid);
+                if (url != undefined) {
+                    console.log('photo fullsizeUrl (from initData):' + url);
+                    sessionStorage.setItem('url_' + tbnid, url);
+                } else {
+                    url = extractHookedData(tbnid);
+                    if (url != undefined) {
+                        console.log('photo fullsizeUrl (from hookedData):' + url);
+                        sessionStorage.setItem('url_' + tbnid, url);
+                    }
+                }
+            }
 
            links = $(this).find('a');
            if (links.length > 0) {
