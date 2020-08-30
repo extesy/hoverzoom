dcb26d91d4f3c41171fdaa4ac75b3601b6de9b8e
GrosPoulet <grosnaingrincheux@gmail.com>
2020-08-30 20:08:27 +0200

    Fix for plug-in : gameblog.fr

--- plugins/gameblog.js
+++ plugins/gameblog.js
@@ -1,19 +1,28 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Gameblog.fr',
-    version:'0.1',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
+        
         hoverZoom.urlReplace(res,
             'img[src*="/tn/"], img[src*="/med/"]',
             /\/(tn|med)\//,
             '/'
         );
+        
         hoverZoom.urlReplace(res,
             'img[src*="mygameblog/av"]',
             /.*\/av(\d+)_.*/,
             'http://www.gameblog.fr/forum/uploads/av-$1.jpg'
         );
-        callback($(res));
+		
+		hoverZoom.urlReplace(res,
+            'img[src]',
+            ['/hi/', '-thumb'],
+            ['/', '']
+        );		
+		
+        callback($(res), this.name);
     }
 });
