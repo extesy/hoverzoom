186b75680ced00c44d735f56451fd5f83e599237
GrosPoulet <grosnaingrincheux@gmail.com>
2020-09-03 17:49:53 +0200

    Update plug-in : Gamespot

--- plugins/gamespot.js
+++ plugins/gamespot.js
@@ -1,19 +1,34 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'GameSpot',
-    version:'0.1',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'a img[src*="_gum"]:not([src*="features/"]), a img[src*="_embed"], a img[src*="thumb"]:not([src*="features/"])',
             /_\d*(gum|thumb|embed)/,
             '_screen'
         );
+
         hoverZoom.urlReplace(res,
             'a:not([href*="/iphone/"]):not([href*="/android/"]):not([href*="/blackberry/"]):not([href*="/palm-webos/"]):not([href*="/windows-mobile/"]) img[src*="/boxshots"]',
             [/\d+\/all\/boxshots\d/, /\/(\d+)(\d)(_\d+)\.jpg/],
             ['bigboxshots', '/$2/$1$2$3_front.jpg']
         );
-        callback($(res));
+
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /(screen|scale|square)_(avatar|medium|mini|petite|small|tiny|large)/,
+            'scale_super'
+        );
+
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /(screen|scale|square)_(avatar|medium|mini|petite|small|tiny|large)/,
+            'original'
+        );
+
+        callback($(res), this.name);
     }
 });
