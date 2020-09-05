9c1d6ed64fb494558c879d73bf1528e978f523cc
GrosPoulet <grosnaingrincheux@gmail.com>
2020-09-05 12:11:42 +0200

    Fix for plug-in : last.fm

--- plugins/lastfm.js
+++ plugins/lastfm.js
@@ -1,7 +1,7 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Last.fm',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
         hoverZoom.urlReplace(res,
@@ -21,6 +21,56 @@ hoverZoomPlugins.push({
             '$1.$2',
             ':eq(0)'
         );
-        callback($(res));
+
+        // /123x456/ -> /
+        var reNxN = /\/\d+[xX]\d+\//;
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            reNxN,
+            '/'
+        );
+
+        // /avatar300s -> /
+        var reAvatar=/\/[a-zA-Z]{0,}\d+s\//;
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            reAvatar,
+            '/'
+        );
+
+        // /arG/ -> /
+        var reARG=/\/arG\//;
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            reARG,
+            '/'
+        );
+
+        $('[style*=background]').each(function() {
+            var link = $(this);
+            // extract url from style
+            var backgroundImage = this.style.backgroundImage;
+            if (backgroundImage.indexOf("url") != -1) {
+                var reUrl = /.*url\s*\(\s*(.*)\s*\).*/i
+                backgroundImage = backgroundImage.replace(reUrl, '$1');
+                // remove leading & trailing quotes
+                var backgroundImageUrl = backgroundImage.replace(/^['"]/,"").replace(/['"]+$/,"");
+
+                $([reNxN, reAvatar, reARG]).each(function() {
+
+                    var fullsizeUrl = backgroundImageUrl.replace(this, '/');
+                    if (fullsizeUrl != backgroundImageUrl) {
+
+                        if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
+                        if (link.data().hoverZoomSrc.indexOf(fullsizeUrl) == -1) {
+                            link.data().hoverZoomSrc.unshift(fullsizeUrl);
+                            res.push(link);
+                        }
+                    }
+                });
+            }
+        });
+
+        callback($(res), this.name);
     }
 });
