40c9313dc5cc2ce360c9934d36b26ec33de950ca
GrosPoulet <grosnaingrincheux@gmail.com>
2020-09-10 18:07:50 +0200

    New plug-in : picturepush.com

---
+++ plugins/picturepush.js
@@ -0,0 +1,14 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'Picturepush.com',
+    version:'1.0',
+    prepareImgLinks:function (callback) {
+        var res = [];
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /(.*)\/photo\/a\/(\d+)\/(.*)\/(.*)/,
+            '$1/photo/a/$2/1024/$4'
+        );
+        callback($(res), this.name);
+    }
+});
