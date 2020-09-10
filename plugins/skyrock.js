886d71b9b3c5d0951d90151d6c38b9b5bde6552b
GrosPoulet <grosnaingrincheux@gmail.com>
2020-09-10 20:07:26 +0200

    Update for plug-in : skyrock.com

--- plugins/skyrock.js
+++ plugins/skyrock.js
@@ -4,21 +4,41 @@ hoverZoomPlugins.push({
     version:'0.1',
     prepareImgLinks:function (callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'a img[src*="small"]',
             ['/small.', '_small_'],
             ['/big.', '_']
         );
+
         hoverZoom.urlReplace(res,
             'a img[src*="_avatar_"]',
             '_avatar_',
             '_'
         );
+
         hoverZoom.urlReplace(res,
             '#photolist a img[src*="big"]',
             /\?.*$/,
             ''
         );
-        callback($(res));
+
+        //sample url:
+        //https://i.skyrock.net/0985/88950985/pics/3331887376_1_38_dKzpHPFD.jpg
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            ['_0_', '_1_', '.0.', '.1.'],
+            ['_2_', '_2_', '.2.', '.2.']
+        );
+
+        //sample url:
+        //https://wir.skyrock.net/wir/v1/profilcrop/?c=isi&im=/0985/88950985/pics/3331887376_1_32_tqV5beqA.jpg&w=264&h=198
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /http.*im=(.*?)(_|\.).(_|\.)(.*?)&(.*)/,
+            'https://mgl.skyrock.net$1$22$3$4'
+        );
+
+        callback($(res), this.name);
     }
 });
