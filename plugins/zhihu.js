ff3da7286a2c1df615f37fb2620cc922f44d3cdc
GrosPoulet <grosnaingrincheux@gmail.com>
2020-09-14 15:56:48 +0200

    Update for plug-in : Zhihu

--- plugins/zhihu.js
+++ plugins/zhihu.js
@@ -1,13 +1,30 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
     name: 'Zhihu',
+    version:'1.1',
     prepareImgLinks: function(callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'img[src*="zhimg.com"]',
             /_(m|s|is)/,
             '_l'
         );
-        callback($(res));
+
+        // image_xs.jpg -> image.jpg
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /_[a-z]{1,}\./,
+            '.'
+        );
+
+        // image_123x456.jpg -> image.jpg
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /_\d+x\d+\./,
+            '.'
+        );
+
+        callback($(res), this.name);
     }
 });
