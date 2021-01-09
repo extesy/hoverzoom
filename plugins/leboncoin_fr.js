7bc0a7f92c8f026d501e5d9228ea812c2e4d2253
GrosPoulet <grosnaingrincheux@gmail.com>
2021-01-09 12:14:07 +0100

    Fix for plug-in : LeBonCoin

--- plugins/leboncoin_fr.js
+++ plugins/leboncoin_fr.js
@@ -1,7 +1,7 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Le bon coin',
-    version:'0.2',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
 
@@ -18,7 +18,15 @@ hoverZoomPlugins.push({
             /\/ad-.*?\//,
             '/ad-large/'
         );
+        
+        //sample url : https://img.leboncoin.fr/api/v1/tenants/9a6387a1-6259-4f2c-a887-7e67f23dd4cb/domains/20bda58f-d650-462e-a72a-a5a7ecf2bf88/buckets/24265337-e7cc-489d-80fc-0c564d62a63b/images/c3/94/77/c39477a76bfe43ba5a164ffda5556324726f0c52.jpg?rule=ad-thumb
+        //          -> https://img.leboncoin.fr/api/v1/tenants/9a6387a1-6259-4f2c-a887-7e67f23dd4cb/domains/20bda58f-d650-462e-a72a-a5a7ecf2bf88/buckets/24265337-e7cc-489d-80fc-0c564d62a63b/images/c3/94/77/c39477a76bfe43ba5a164ffda5556324726f0c52.jpg?rule=ad-large
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /rule=ad-.*/,
+            'rule=ad-large'
+        );
 
-        callback($(res));
+        callback($(res), this.name);
     }
 });
