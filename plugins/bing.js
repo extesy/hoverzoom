cfd9f8a8ad525351fb0a24666c747fa530ba41ce
GrosPoulet <grosnaingrincheux@gmail.com>
2021-01-09 11:53:05 +0100

    Fix for plug-in : Bing

--- plugins/bing.js
+++ plugins/bing.js
@@ -1,7 +1,7 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Bing',
-    version:'0.2',
+    version:'0.3',
     prepareImgLinks:function (callback) {
 
         var res = [];
@@ -10,13 +10,22 @@ hoverZoomPlugins.push({
             var link = $(this),
                 m = this.getAttribute('m') || '',
                 m1 = m.replace(/([{|,])([a-zA-Z0-9]+)\:/g, '$1"$2":'),
+                m2 = {};
+            try {
                 m2 = $.parseJSON(m1);
+            } catch {}
             var url = m2.murl;
             if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
             if (url && link.data().hoverZoomSrc.indexOf(url) == -1) {
                 link.data().hoverZoomSrc.unshift(url);
-                if (m2.t) { link.data().hoverZoomCaption = m2.t }
-                link.data().href = m2.purl;
+                var href = m2.purl;
+                link.data().href = href;
+                var meta = link.data().meta || {};
+                try {
+                    if (m2.t) {  link.data().hoverZoomCaption = m2.t; meta.text = m2.t; }
+                    if (m2.desc) meta.desc = m2.desc;
+                } catch {}
+                link.data().meta = meta;
                 res.push(link);
             }
         });
@@ -32,6 +41,6 @@ hoverZoomPlugins.push({
             }
         });
 
-        callback($(res));
+        callback($(res), this.name);
     }
 });
