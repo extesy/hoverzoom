f058172fd80ee96b947f2dba3084f4496dc2937b
Oleg Anashkin <oleg.anashkin@gmail.com>
2021-01-29 20:27:09 -0800

    Support for new imdb design

--- plugins/imdb.js
+++ plugins/imdb.js
@@ -8,6 +8,8 @@ hoverZoomPlugins.push({
             var elem = $(this),
                 url = elem.attr('loadlate') || hoverZoom.getThumbUrl(this);
             url = url.replace(/\._V1.*\./, options.showHighRes ? '.' : '._V1._SX600_SY600_.');
+            var target = elem.parents('div[role="group"]');
+            if (target.length > 0) elem = $(target[0]);
             elem.data().hoverZoomSrc = [url];
             res.push(elem);
         });
