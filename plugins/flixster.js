e57f5db5d8016341cb163a5cc31e890628266663
GrosPoulet <grosnaingrincheux@gmail.com>
2021-02-08 20:44:03 +0100

    Plug-ins enhancements + 1 new plug-in: Medium_a

--- plugins/flixster.js
+++ plugins/flixster.js
@@ -1,14 +1,50 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Flixster',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
+        
         hoverZoom.urlReplace(res,
             'a img[src*="_tmb."]:not([src*="/critic/"]), a img[src*="_msq."], a img[src*="_pro."]',
             /_(tmb|msq|pro)\./,
             '_gal.'
         );
-        callback($(res));
+        
+        //sample url: https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/300x0/v2/https://flxt.tmsimg.com/v9/AllPhotos/488370/488370_v9_ba.jpg
+        // -> https://flxt.tmsimg.com/v9/AllPhotos/488370/488370_v9_ba.jpg
+        var reShorten = /(.*)http(.*)/;
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            reShorten,
+            'http$2'
+        );
+        
+        $('[style*=background]').each(function() {
+            var link = $(this);
+            // extract url from style
+            var backgroundImage = this.style.backgroundImage;
+            if (backgroundImage.indexOf("url") != -1) {
+                var reUrl = /.*url\s*\(\s*(.*)\s*\).*/i
+                backgroundImage = backgroundImage.replace(reUrl, '$1');
+                // remove leading & trailing quotes
+                var backgroundImageUrl = backgroundImage.replace(/^['"]/,"").replace(/['"]+$/,"");
+                
+                $([reShorten]).each(function() {
+              
+                    var fullsizeUrl = backgroundImageUrl.replace(this, 'http$2');
+                    if (fullsizeUrl != backgroundImageUrl) {  
+                        
+                        if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
+                        if (link.data().hoverZoomSrc.indexOf(fullsizeUrl) == -1) { 
+                            link.data().hoverZoomSrc.unshift(fullsizeUrl);
+                            res.push(link); 
+                        }
+                    }
+                });
+            }
+        });
+        
+        callback($(res), this.name);
     }
 });
--- plugins/gravatar_a.js
+++ plugins/gravatar_a.js
@@ -1,24 +1,25 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Gravatar',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var i, url, res = [],
             img, imgs = qsa('img[src*="gravatar.com/avatar/"]');
         for (i = 0; i < imgs.length; i++) {
             img = imgs[i];
             if (img.src.indexOf('?') == -1) {
-                url = img.src + '?s=420';
+                url = img.src + '?s=512';
             } else if (img.src.indexOf('s=') > -1) {
-                url = img.src.replace(/s=\d+/, 's=420');
+                url = img.src.replace(/s=\d+/, 's=512');
             } else {
-                url = img.src + '&s=420';
+                url = img.src + '&s=512';
             }
             img = $(img);
             img.data().hoverZoomSrc = [url];
             res.push(img);
         }
         if (res.length) {
-            callback($(res));
+            callback($(res), this.name);
         }
     }
 });
