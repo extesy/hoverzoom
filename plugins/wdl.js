5c41a31d0b843eb8d41badd4bffa0aa29f66eaf2
GrosPoulet <grosnaingrincheux@gmail.com>
2021-04-09 10:28:41 +0200

    New plug-ins: Duitang, LibraryOfCongress, WorldDigitalLibrary

---
+++ plugins/wdl.js
@@ -0,0 +1,16 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'WDL',
+    version:'0.1',
+    prepareImgLinks:function (callback) {
+        var res = [];
+
+        hoverZoom.urlReplace(res,
+            'img[src],[style*=url]',
+            /\d+x\d+/,
+            '1024x1024'
+        );
+
+        callback($(res), this.name);
+    }
+});
