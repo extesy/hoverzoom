fdb187c820a3741f9cba8257eac1f00de66f4a9d
Mattwmaster58 <mattwmaster58@gmail.com>
2021-06-12 11:11:33 -0400

    syntax error

--- js/hoverzoom.js
+++ js/hoverzoom.js
@@ -450,7 +450,7 @@ var hoverZoom = {
             titledElements = img.parents('[title]').not('iframe, .lightbox, [rel^="lightbox"]');
             // Occasionally, the img element itself will have a title attribute
             // which we like to  handle that as well
-            titledElements = titledElements.add(img.filter('[title]');
+            titledElements = titledElements.add(img.filter('[title]'));
             titledElements.each(function () {
                 $(this).data().hoverZoomTitle = this.getAttribute('title');
                 this.title = '';
