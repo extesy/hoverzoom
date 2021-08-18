20230c9a5c60d60a50d23cb4c7e081d354b92abd
Oleg Anashkin <oleg.anashkin@gmail.com>
2021-08-18 16:31:28 -0700

    Update release script to include all locales

--- release.cmd
+++ release.cmd
@@ -1,3 +1,4 @@
 @echo off
 del hoverzoom.zip >nul
-winrar a -afzip -m5 -r hoverzoom.zip _locales\ar _locales\bg _locales\de _locales\en _locales\es _locales\fa _locales\fr _locales\hi _locales\hr _locales\hu _locales\id _locales\it _locales\ja _locales\ko _locales\nl _locales\no _locales\pl _locales\pt_BR _locales\pt_PT _locales\ru _locales\sv _locales\th _locales\tr _locales\zh_CN css fonts html images js plugins LICENSE manifest.json -xnode_modules -x.idea
+winrar a -afzip -m5 -r hoverzoom.zip _locales css fonts html images js plugins LICENSE manifest.json -xnode_modules -x.idea
+
