a83cdaecce9ce2b4b455f6641145ec4ccc16817f
GrosPoulet <grosnaingrincheux@gmail.com>
2021-12-31 18:38:26 +0100

    New plug-in for: WikiFeet

---
+++ plugins/wikifeet.js
@@ -0,0 +1,22 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'Wikifeet',
+    version:'0.1',
+    prepareImgLinks:function (callback) {
+        var res = [];
+
+        hoverZoom.urlReplace(res,
+            'img[src*="wikifeet"],[style*="url"]',
+            'thumbs.',
+            'pics.'
+        );
+
+        hoverZoom.urlReplace(res,
+            'img[src*="/t"],[style*="url"]',
+            /\/t(\d+)/,
+            '/$1'
+        );
+
+        callback($(res), this.name);
+    }
+});
