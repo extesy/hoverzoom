d4b06f893ecc5607c54f38c1a253b23a20a9529e
GrosPoulet <grosnaingrincheux@gmail.com>
2022-01-09 18:22:18 +0100

    Fix for plug-in: GFYCAT

--- plugins/gfycat.js
+++ plugins/gfycat.js
@@ -1,7 +1,10 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'gfycat.com',
+    version:'0.2',
     prepareImgLinks:function (callback) {
+        var res = [];
+
         $('a[href*="gfycat.com/"]').one('mouseenter', function() {
             var link = $(this),
                 gfyId = this.href.replace(/.*gfycat.com\/(..\/)?(gifs\/)?(detail\/)?(\w+).*/, '$4');
@@ -21,5 +24,34 @@ hoverZoomPlugins.push({
                     })
                 });
         });
+
+        // sample:   https://thumbs.gfycat.com/BronzePiercingCrownofthornsstarfish-mobile.jpg
+        // fullsize: https://zippy.gfycat.com/BronzePiercingCrownofthornsstarfish.mp4
+        hoverZoom.urlReplace(res,
+            'a img[src*="thumbs"]',
+            /https:\/\/thumbs\.gfycat\.com\/([^.-]{1,}).*/,
+            'https://zippy.gfycat.com/$1.mp4',
+            'a'
+        );
+
+        // sample:   https://thumbs.gfycat.com/AstonishingThatGrizzlybear-mobile.jpg
+        // fullsize: https://fat.gfycat.com/AstonishingThatGrizzlybear.mp4
+        hoverZoom.urlReplace(res,
+            'a img[src*="thumbs"]',
+            /https:\/\/thumbs\.gfycat\.com\/([^.-]{1,}).*/,
+            'https://fat.gfycat.com/$1.mp4',
+            'a'
+        );
+
+        // sample:   https://thumbs.gfycat.com/VagueSingleGermanwirehairedpointer-mobile.jpg
+        // fullsize: https://giant.gfycat.com/VagueSingleGermanwirehairedpointer.mp4
+        hoverZoom.urlReplace(res,
+            'a img[src*="thumbs"]',
+            /https:\/\/thumbs\.gfycat\.com\/([^.-]{1,}).*/,
+            'https://giant.gfycat.com/$1.mp4',
+            'a'
+        );
+
+        callback($(res), this.name);
     }
 });
