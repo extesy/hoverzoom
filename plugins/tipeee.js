ec638ba0b705c20cd9b267aeb0e9d2f91773ed94
GrosPoulet <grosnaingrincheux@gmail.com>
2022-01-28 11:32:05 +0100

    New plug-in for: Tipeee

---
+++ plugins/tipeee.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'Tipeee',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var res = [];
+
+        // sample:   https://www.tipeeestream.com/cdn-cgi/image/onerror=redirect,width=400,height=400,fit=cover/https://api.tipeee.com/uploads/media/image/png/20211019/20211019616f1c0af1baf.png
+        // fullsize: https://api.tipeee.com/uploads/media/image/png/20211019/20211019616f1c0af1baf.png
+        hoverZoom.urlReplace(res,
+            'img[src],[style*="url"]',
+            /.*\/(http.*)/,
+            '$1'
+        );
+
+        callback($(res), this.name);
+    }
+});
