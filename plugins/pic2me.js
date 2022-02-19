569923a5c483b1ac982d05eb2952f54a13689167
GrosPoulet <grosnaingrincheux@gmail.com>
2022-02-19 15:21:28 +0100

    New plug-in for: pic2.me

---
+++ plugins/pic2me.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'pic2me',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var res = [];
+
+        // sample:   https://storge.pic2.me/w/280x175/593/55dcdfc79cc19.webp
+        // fullsize: https://storge.pic2.me/upload/593/55dcdfc79cc19.jpg
+        hoverZoom.urlReplace(res,
+            'img[src*="pic2.me"]',
+            /\/.\/\d+x\d+\/(.*)\.(.*)/,
+            '/upload/$1.jpg'
+        );
+
+        callback($(res), this.name);
+    }
+});
