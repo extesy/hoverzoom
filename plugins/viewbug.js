b52fded19ef6d3432d474e64e702666584f9988a
GrosPoulet <grosnaingrincheux@gmail.com>
2022-03-27 19:42:22 +0200

    New plug-in for: viewbug.com (#896)
    + fix for image/backgroundImage conflics.
    This (rare) situation occurs on viewbug.com

---
+++ plugins/viewbug.js
@@ -0,0 +1,28 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'viewbug',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var res = [];
+
+        // sample:   https://www.viewbug.com/media/images/contests/contest8467_35x35.jpeg
+        // fullsize: https://www.viewbug.com/media/images/contests/contest8467_banner.jpeg
+        hoverZoom.urlReplace(res,
+            'img[src],div',
+            /(contest.*)_(.*)\./,
+            '$1_banner.',
+            'a'
+        );
+
+        // sample:   https://cdnpt01.viewbug.com/media/mediafiles/2019/10/29/86898663_380x380.jpg
+        // fullsize: https://cdnpt01.viewbug.com/media/mediafiles/2019/10/29/86898663_large1600.jpg
+        hoverZoom.urlReplace(res,
+            'img[src],div',
+            /_(.*)\./,
+            '_large1600.',
+            'a'
+        );
+
+        callback($(res), this.name);
+    }
+});
