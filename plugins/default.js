e70419e59ea89f71b60feced64d299539987e10c
GrosPoulet <grosnaingrincheux@gmail.com>
2022-06-18 10:54:53 +0200

    Support for playlists (M3U8)
    Not so many of them out there on the internet, samples can be found here: https://ottverse.com/free-hls-m3u8-test-urls/

--- plugins/default.js
+++ plugins/default.js
@@ -1,11 +1,12 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name: 'Default',
-    version:'0.4',
+    version:'0.5',
     prepareImgLinks: function (callback) {
         var res = [];
         const reVideos = /\/[^:]+\.(?:3gpp|m4v|mkv|mp4|ogv|webm)(?:[\?#].*)?(?:\/)?$/i
         const reImages = /\/[^:]+\.(?:bmp|gifv?|ico|jpe?g|png|svg|webp|xbm)(?:[\?#].*)?(?:\/)?$/i
+        const rePlaylists = /\/[^:]+\.(?:m3u8)(?:[\?#].*)?(?:\/)?$/i
         $('a[href]').filter(function () {
             if (typeof(this.href) != 'string')
 			    return false;
@@ -14,6 +15,8 @@ hoverZoomPlugins.push({
             if (this.href.match(reImages))
                 return true;
             if (this.href.match(reVideos))
+                return true;
+            if (this.href.match(rePlaylists))
                 return true;
             return false;
         }).each(function () {
