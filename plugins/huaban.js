cca65924336a1f23616b2c131d5f3b4db1126ced
GrosPoulet <grosnaingrincheux@gmail.com>
2022-07-21 14:32:49 +0200

    Fix for plug-in: Huaban (#972)

--- plugins/huaban.js
+++ plugins/huaban.js
@@ -1,25 +1,25 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
     name: 'huaban',
-    version: '1.0',
+    version: '1.1',
     prepareImgLinks: function(callback) {
         var res = [];
 
         // sample:   https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni_fw86/format/webp
-        // fullsize: https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni_
+        // fullsize: https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni
         hoverZoom.urlReplace(res,
             'img[src*="huaban.com"],div',
             /https:\/\/.*hbimg.huaban.com\/(.*)_(.*)/,
-            'https://hbimg.huaban.com/$1_',
+            'https://hbimg.huaban.com/$1',
             'a'
         );
-        
+
         // sample:   https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni_fw86/format/webp
-        // fullsize: https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni_
+        // fullsize: https://hbimg.huaban.com/ee3e69d3e537cbe479c7df83611ab1c056639ac61df2d-KuC9Ni
         hoverZoom.urlReplace(res,
             'img[src*="huaban.com"],div',
             /https:\/\/.*hbimg.huaban.com\/(.*)_(.*)/,
-            'https://hbimg.huaban.com/$1_'
+            'https://hbimg.huaban.com/$1'
         );
 
         callback($(res), this.name);
