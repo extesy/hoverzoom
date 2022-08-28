2eab7bcd70cd39b3893392f87ab40b3f65ca222a
GrosPoulet <grosnaingrincheux@gmail.com>
2022-08-28 12:03:10 +0200

    Fix for plug-in: imageevent.js

--- plugins/imageevent.js
+++ plugins/imageevent.js
@@ -1,15 +1,19 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'imageevent.com',
-    version:'1.0',
+    version:'1.1',
     prepareImgLinks:function (callback) {
 
-        var res = [];       
+        var res = [];
 
+        // page with samples: https://imageevent.com/powerwagon/doorart
+        // thumbnail: https://photos.imageevent.com/powerwagon/doorart/icons/phillips_stores.jpg
+        // fullsize: https://photos.imageevent.com/powerwagon/doorart/phillips_stores.jpg
         hoverZoom.urlReplace(res,
             'img[src]',
             ['/large/', '/icons/', '/small/', '/websize/'],
-            ['/', '/', '/', '/']
+            ['/', '/', '/', '/'],
+            'a'
         );
 
         callback($(res), this.name);
