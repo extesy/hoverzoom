4d391cf374ee07c96025228c29904a26c3842233
GrosPoulet <grosnaingrincheux@gmail.com>
2022-09-11 18:46:09 +0200

    New plug-in for: rule34.xxx & rule34video.com

---
+++ plugins/rule34.js
@@ -0,0 +1,44 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'rule34',
+    version:'1.0',
+    prepareImgLinks:function (callback) {
+
+        var res = [];
+
+        // images
+        // thumbnail: https://wimg.rule34.xxx/thumbnails/5865/thumbnail_0e4386ce2217951233a1272ec3d43ba0ab8592b0.jpg
+        // fullsize:  https://wimg.rule34.xxx/images/5865/0e4386ce2217951233a1272ec3d43ba0ab8592b0.png
+        hoverZoom.urlReplace(res,
+            'img[src]:not([style])',
+            /\/(thumbnails|samples)\/\/?(\d+)\/\/?(thumbnail|sample)_(.*)\.(.*)/,
+            '/images/$2/$4.gif'
+        );
+        hoverZoom.urlReplace(res,
+            'img[src]:not([style])',
+            /\/(thumbnails|samples)\/\/?(\d+)\/\/?(thumbnail|sample)_(.*)\.(.*)/,
+            '/images/$2/$4.jpg'
+        );
+        hoverZoom.urlReplace(res,
+            'img[src]:not([style])',
+            /\/(thumbnails|samples)\/\/?(\d+)\/\/?(thumbnail|sample)_(.*)\.(.*)/,
+            '/images/$2/$4.png'
+        );
+        hoverZoom.urlReplace(res,
+            'img[src]:not([style])',
+            /\/(thumbnails|samples)\/\/?(\d+)\/\/?(thumbnail|sample)_(.*)\.(.*)/,
+            '/images/$2/$4.jpeg'
+        );
+
+        // videos
+        // thumbnail (poster): https://wimg.rule34.xxx/thumbnails/5866/thumbnail_332f750db8eb0a5e1df93666b55428d6.jpg
+        // video:              https://wimg.rule34.xxx/images/5866/332f750db8eb0a5e1df93666b55428d6.mp4
+        hoverZoom.urlReplace(res,
+            'img[src][style]',
+            /\/(thumbnails|samples)\/\/?(\d+)\/\/?(thumbnail|sample)_(.*)\.(.*)/,
+            '/images/$2/$4.mp4'
+        );
+
+        callback($(res), this.name);
+    }
+});
---
+++ plugins/rule34video_a.js
@@ -0,0 +1,48 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'rule34video_a',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var res = [];
+
+        // if header(s) rewrite is allowed store headers settings that will be used for rewrite
+        if (options.allowHeadersRewrite) {
+            chrome.runtime.sendMessage({action:"storeHeaderSettings",
+                                        plugin:name,
+                                        settings:
+                                            [{"type":"request",
+                                            "skipInitiator":"rule34video",
+                                            "url":"rule34video",
+                                            "headers":[{"name":"referer", "value":"https://rule34video.com/", "typeOfUpdate":"add"}]}]
+                                        });
+        }
+
+        // link:  https://rule34video.com/videos/3086641/step-by-step-of-sarada-sound-angel/
+        // video: https://rule34video.com/get_file/6/e50e7782cd629cbbedad233e4b8379cd63d8c56ca4/3086000/3086641/3086641_1080p.mp4
+        $('a[href*="rule34video"]:not(.hoverZoomMouseover)').addClass('hoverZoomMouseover').one('mouseover', function() {
+
+            var link = undefined;
+            var href = undefined;
+
+            href = this.href;
+            link = $(this);
+
+            // clean previous result
+            link.data().hoverZoomSrc = [];
+            hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
+
+                let a = doc.querySelector('a[href*="_1080p.mp4"]');
+                if (a) return a.href.replace(/(^https.*?_1080p.mp4).*/, '$1');
+                a = doc.querySelector('a[href*="_720p.mp4"]');
+                if (a) return a.href.replace(/(^https.*?_720p.mp4).*/, '$1');
+                a = doc.querySelector('a[href*="_480p.mp4"]');
+                if (a) return a.href.replace(/(^https.*?_480p.mp4).*/, '$1');
+                a = doc.querySelector('a[href*="_360.mp4"]');
+                if (a) return a.href.replace(/(^https.*?_360.mp4).*/, '$1');
+
+            }, false); // get source sync
+        });
+
+        callback($(res), this.name);
+    }
+});
