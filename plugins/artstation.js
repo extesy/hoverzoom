46cea5ccc0f5593ff3b683eecf525f74da6cd3e8
Oleg Anashkin <oleg.anashkin@gmail.com>
2022-09-17 14:07:38 -0700

    Update artstation plugin

--- plugins/artstation.js
+++ plugins/artstation.js
@@ -2,21 +2,17 @@ var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'artstation.com',
     version:'0.1',
-    prepareImgLinks:function (callback) {
-        var res = [];   
-        
+
+    prepareImgLinks: function (callback) {
+        const res = [];
+
         hoverZoom.urlReplace(res,
             'img[src*="artstation"]',
             /(\/[^0-9]*\/\d+\/\d+\/\d+\/)(\d+\/)?(small|smaller_square|small_square|micro_square|medium|medium_wide|default)\//,
-            '$1original/'
+            '$1large/',
+            'a.project-image'
         );
-        
-        hoverZoom.urlReplace(res,
-            'img[src*="artstation"]',
-            /(\/[^0-9]*\/\d+\/\d+\/\d+\/)(\d+\/)?(small|smaller_square|small_square|micro_square|medium|medium_wide|default)\//,
-            '$1large/'
-        );
-                
+
         callback($(res));
     }
 });
