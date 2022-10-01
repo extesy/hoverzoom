f1a52c599c00ce3488ccdfd3cb48873c7c0d5153
GrosPoulet <grosnaingrincheux@gmail.com>
2022-10-01 16:47:31 +0200

    Fix for TikTok plug-ins
    When enabled, TikTok plug-ins prevented users from posting comments & liking.

--- plugins/tiktok_a.js
+++ plugins/tiktok_a.js
@@ -1,7 +1,7 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push( {
     name: 'tiktok_a',
-    version: '1.2',
+    version: '1.3',
     prepareImgLinks: function(callback) {
         var name = this.name;
 
@@ -10,16 +10,15 @@ hoverZoomPlugins.push( {
 
         function getVideo(videoId, jsonData, link) {
             try {
-                let j = JSON.parse(jsonData);
+                const j = JSON.parse(jsonData);
                 let audioUrl = j["itemInfo"] ? j["itemInfo"]["itemStruct"]["music"]["playUrl"] : j["ItemModule"][videoId]["music"]["playUrl"];
                 if (audioUrl) audioUrl += '.audiomuted'; // there is already a soundtrack in video, this one is only for download
-                let videoUrlPlay = j["itemInfo"] ? j["itemInfo"]["itemStruct"]["video"]["playAddr"] : j["ItemModule"][videoId]["video"]["playAddr"];
-                let videoUrlDownload = j["itemInfo"] ? j["itemInfo"]["itemStruct"]["video"]["downloadAddr"] : j["ItemModule"][videoId]["video"]["downloadAddr"];
+                const videoUrlPlay = j["itemInfo"] ? j["itemInfo"]["itemStruct"]["video"]["playAddr"] : j["ItemModule"][videoId]["video"]["playAddr"];
+                const videoUrlDownload = j["itemInfo"] ? j["itemInfo"]["itemStruct"]["video"]["downloadAddr"] : j["ItemModule"][videoId]["video"]["downloadAddr"];
                 let videoUrl = (videoUrlDownload ? videoUrlDownload : videoUrlPlay);
                 if (videoUrl) videoUrl += '.video';
                 if (videoUrl) {
-                    let urlVideoAudio;
-                    urlVideoAudio = (audioUrl ? videoUrl + "_" + audioUrl : videoUrl);
+                    const urlVideoAudio = (audioUrl ? videoUrl + "_" + audioUrl : videoUrl);
                     link.data().hoverZoomTikTokVideoUrl = urlVideoAudio;
                     link.data().hoverZoomSrc = [urlVideoAudio];
                     callback(link, name);
@@ -27,33 +26,32 @@ hoverZoomPlugins.push( {
                 }
             } catch {}
         }
+
         // load user page then extract user picture url
         // proceed with API call from background page
         function getUserPictureFromPage(userUrl, userId, link) {
-
             chrome.runtime.sendMessage({action:'ajaxRequest',
                                         method:'GET',
                                         url:userUrl,
                                         headers:[{"header":"Content-Type","value":"application/json"}]},
 
                                         function (response) {
-
                                             if (response == null) { return; }
 
                                             // extract JSON data
-                                            let index1 = response.indexOf(token1);
+                                            const index1 = response.indexOf(token1);
                                             if (index1 == -1) { return; }
-                                            let index2 = response.indexOf(token2, index1);
+                                            const index2 = response.indexOf(token2, index1);
                                             if (index2 == -1) { return; }
-                                            let index3 = response.indexOf('{', index1);
-                                            let index4 = response.lastIndexOf('}', index2);
-                                            let jsonData = response.substring(index3, index4 + 1);
+                                            const index3 = response.indexOf('{', index1);
+                                            const index4 = response.lastIndexOf('}', index2);
+                                            const jsonData = response.substring(index3, index4 + 1);
                                             try {
-                                                let j = JSON.parse(jsonData);
-                                                let avatarUrl = j["UserModule"]["users"][userId]["avatarLarger"];
+                                                const j = JSON.parse(jsonData);
+                                                const avatarUrl = j["UserModule"]["users"][userId]["avatarLarger"];
 
                                                 if (avatarUrl) {
-                                                    link.data().hoverZoomTikTokUserUrl = avatarUrl;
+                                                    link.data().hoverZoomTikTokAvatarUrl = avatarUrl;
                                                     link.data().hoverZoomSrc = [avatarUrl];
                                                     callback(link, name);
                                                     hoverZoom.displayPicFromElement(link);
@@ -66,25 +64,22 @@ hoverZoomPlugins.push( {
         // sample: https://www.tiktok.com/@francerugby
         $('a[href*="/@"]').filter(function() { return (/www.tiktok.com\/@[^\/]{1,}$/.test($(this).prop('href'))) }).on('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
-
-            href = this.href;
-            link = $(this);
+            const href = this.href;
+            const link = $(this);
 
             const re1 = /www.tiktok.com\/@([^\/?]{1,})/;   // user id (francerugby)
-            let m = href.match(re1);
+            const m = href.match(re1);
             if (m == undefined) return;
-            let userId = m[1];
+            const userId = m[1];
 
             // resuse previous result
             if (link.data().hoverZoomTikTokUserId == userId) {
-                if (link.data().hoverZoomTikTokUserUrl) link.data().hoverZoomSrc = [link.data().hoverZoomTikTokUserUrl];
+                if (link.data().hoverZoomTikTokAvatarUrl) link.data().hoverZoomSrc = [link.data().hoverZoomTikTokAvatarUrl];
                 return;
             }
 
             link.data().hoverZoomTikTokUserId = userId;
-            link.data().hoverZoomTikTokUserUrl = undefined;
+            link.data().hoverZoomTikTokAvatarUrl = undefined;
 
             // clean previous result
             link.data().hoverZoomSrc = [];
@@ -94,7 +89,6 @@ hoverZoomPlugins.push( {
         // load video page then extract video url
         // proceed with API call from background page
         function getVideoFromPage(videoUrl, videoId, link) {
-
             chrome.runtime.sendMessage({action:'ajaxRequest',
                                         method:'GET',
                                         url:videoUrl,
@@ -105,15 +99,15 @@ hoverZoomPlugins.push( {
                                             if (response == null) { return; }
 
                                             // extract JSON data
-                                            let index1 = response.indexOf(token1);
+                                            const index1 = response.indexOf(token1);
                                             if (index1 == -1) { return; }
-                                            let index2 = response.indexOf(token2, index1);
+                                            const index2 = response.indexOf(token2, index1);
                                             if (index2 == -1) { return; }
-                                            let index3 = response.indexOf('{', index1);
-                                            let index4 = response.lastIndexOf('}', index2);
-                                            let jsonData = response.substring(index3, index4 + 1);
+                                            const index3 = response.indexOf('{', index1);
+                                            const index4 = response.lastIndexOf('}', index2);
+                                            const jsonData = response.substring(index3, index4 + 1);
                                             if (videoId == undefined) {
-                                                var m = jsonData.match(/"video":{"id":"(\d+)"/);
+                                                const m = jsonData.match(/"video":{"id":"(\d+)"/);
                                                 videoId = m[1];
                                             }
                                             getVideo(videoId, jsonData, link);
@@ -124,16 +118,13 @@ hoverZoomPlugins.push( {
         // sample: https://vm.tiktok.com/TTPdkHATKx/
         $('a[href*="vm.tiktok.com/"],a[href*="vt.tiktok.com/"]').on('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
-
-            href = this.href;
-            link = $(this);
+            const href = this.href;
+            const link = $(this);
 
             const re = /v[mt].tiktok.com\/([^\/?]{1,})/;   // pseudo video id (e.g. TTPdkHATKx)
-            let m = href.match(re);
+            const m = href.match(re);
             if (m == undefined) return;
-            let pseudoVideoId = m[1];
+            const pseudoVideoId = m[1];
 
             // resuse previous result
             if (link.data().hoverZoomTikTokVideoId == pseudoVideoId) {
@@ -157,21 +148,18 @@ hoverZoomPlugins.push( {
         // => video: https://v16-webapp.tiktok.com/cc389c6a68701f5a2e56fe39b2206a0d/61fc68db/video/tos/useast2a/tos-useast2a-pve-0068/09d6f7e3e4e64b54890746c1b5df9a2f/?a=1988&br=2326&bt=1163&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3D_nz7TheIAxlXq&l=202202031744150102231230461D200629&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajhyZzg6ZmhmOjMzNzczM0ApPDpnaTk3NmQ5N2VkZjc7NWc2YjMzcjRvbm1gLS1kMTZzc2EvLzYzXzJhYWFeL2FhYWI6Yw%3D%3D&vl=&vr=
         $('a[href*="/@"]').filter(function() { return (/www.tiktok.com\/@.*\/video\/\d+/.test($(this).prop('href'))) }).on('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
-
-            href = this.href;
-            link = $(this);
+            const href = this.href;
+            const link = $(this);
 
             const re1 = /www.tiktok.com\/@(.*)\/video\/\d+/;   // user id (e.g. colexicano)
             let m = href.match(re1);
             if (m == undefined) return;
-            let userId = m[1];
+            const userId = m[1];
 
             const re2 = /www.tiktok.com\/@.*\/video\/(\d+)/;   // video id (e.g. 7013483135040032005)
             m = href.match(re2);
             if (m == undefined) return;
-            let videoId = m[1];
+            const videoId = m[1];
 
             // resuse previous result
             if (link.data().hoverZoomTikTokVideoId == videoId) {
@@ -196,20 +184,18 @@ hoverZoomPlugins.push( {
                                         method:'GET',
                                         url:userUrl,
                                         headers:[{"header":"Content-Type","value":"application/json"}]},
-
                                         function (response) {
-
                                             // extract JSON data
-                                            let index1 = response.indexOf(token1);
+                                            const index1 = response.indexOf(token1);
                                             if (index1 == -1) { return; }
-                                            let index2 = response.indexOf(token2, index1);
+                                            const index2 = response.indexOf(token2, index1);
                                             if (index2 == -1) { return; }
-                                            let index3 = response.indexOf('{', index1);
-                                            let index4 = response.lastIndexOf('}', index2);
-                                            let jsonData = response.substring(index3, index4 + 1);
+                                            const index3 = response.indexOf('{', index1);
+                                            const index4 = response.lastIndexOf('}', index2);
+                                            const jsonData = response.substring(index3, index4 + 1);
                                             try {
-                                                let j = JSON.parse(jsonData);
-                                                let roomId = j["UserModule"]["users"][userId]["roomId"];
+                                                const j = JSON.parse(jsonData);
+                                                const roomId = j["UserModule"]["users"][userId]["roomId"];
                                                 if (roomId == undefined) return; // no live to display
 
                                                 chrome.runtime.sendMessage({action:'ajaxRequest',
@@ -222,8 +208,8 @@ hoverZoomPlugins.push( {
                                                     if (response == null) { return; }
 
                                                     try {
-                                                        let l = JSON.parse(response);
-                                                        let hls_pull_url = l['data']['stream_url'].hls_pull_url;
+                                                        const l = JSON.parse(response);
+                                                        const hls_pull_url = l['data']['stream_url'].hls_pull_url;
                                                         if (hls_pull_url) {
                                                             link.data().hoverZoomTikTokLiveUrl = hls_pull_url;
                                                             link.data().hoverZoomSrc = [hls_pull_url];
@@ -242,16 +228,13 @@ hoverZoomPlugins.push( {
         // sample: https://www.tiktok.com/@benoit_chevalier/live
         $('a[href*="/@"]').filter(function() { return (/www.tiktok.com\/@.*\/live/.test($(this).prop('href'))) }).on('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
-
-            href = this.href;
-            link = $(this);
+            const href = this.href;
+            const link = $(this);
 
             const re1 = /www.tiktok.com\/@([^\/?]{1,})/;   // user id (e.g. benoit_chevalier)
-            let m = href.match(re1);
+            const m = href.match(re1);
             if (m == undefined) return;
-            let userId = m[1];
+            const userId = m[1];
 
             // resuse previous result
             if (link.data().hoverZoomTikTokUserId == userId) {
@@ -271,16 +254,13 @@ hoverZoomPlugins.push( {
         // sample: https://www.tiktok.com/api/img/?itemId=7064678143478566190&location=0&aid=1988
         $('a[href*="itemId"]').filter(function() { return (/www.tiktok.com\/.*\/\?itemId=\d+/.test($(this).prop('href'))) }).on('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
-
-            href = this.href;
-            link = $(this);
+            const href = this.href;
+            const link = $(this);
 
             const re1 = /www.tiktok.com\/.*\/\?itemId=(\d+)/;   // video id (e.g. 7064678143478566190)
-            let m = href.match(re1);
+            const m = href.match(re1);
             if (m == undefined) return;
-            let videoId = m[1];
+            const videoId = m[1];
 
             // resuse previous result
             if (link.data().hoverZoomTikTokVideoId == videoId) {
