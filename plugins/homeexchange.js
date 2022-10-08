4c023ce20bd88d849f3baa96438af964a20f8e9f
GrosPoulet <grosnaingrincheux@gmail.com>
2022-10-08 16:36:50 +0200

    Enhancement for plug-in: HomeExchange

--- plugins/homeexchange.js
+++ plugins/homeexchange.js
@@ -1,9 +1,11 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
   name: 'homeexchange',
-  version: '0.1',
+  version: '0.2',
   prepareImgLinks: (callback) => {
     let res = [];
+    const re = /\?.[^"]*/;
+    const patch = '?quality=100&height=1080&width=1080';
 
     // Example URLs:
     //    https://image.homeexchange.fr/images/home/3694222/1887472/1698493326865718.jpg?quality=80&keep-ratio=true
@@ -12,30 +14,37 @@ hoverZoomPlugins.push({
     //    https://image.homeexchange.fr/images/home/2859575/1483460/1719250638093108.jpg?quality=90&height=1100&keep-ratio=true
     hoverZoom.urlReplace(
       res,
-      'a[data-picture*="image.homeexchange"]',
-      /\?.[^"]*/,
-      '?quality=100&height=800&smart-resize=true&smart-resize=true'
+      'a[data-picture*="/images/home/"], a[data-picture*="/images/user/"]',
+      re,
+      patch
     );
 
     hoverZoom.urlReplace(
       res,
-      '[data-bg*="image.homeexchange"]',
-      /\?.[^"]*/,
-      '?quality=100&height=800&smart-resize=true&smart-resize=true'
+      '[data-bg*="/images/home/"], [data-bg*="/images/user/"]',
+      re,
+      patch
     );
 
     hoverZoom.urlReplace(
       res,
-      'img[src*="image.homeexchange"]',
-      /\?.[^"]*/,
-      '?quality=100&width=500&height=500&smart-resize=true&smart-resize=true'
+      'img[src*="/images/home/"], img[src*="/images/user/"]',
+      re,
+      patch
     );
 
     hoverZoom.urlReplace(
       res,
-      '.user-image[style*="image.homeexchange"]',
-      /\?.[^"]*/,
-      '?quality=100&width=500&height=500&smart-resize=true&smart-resize=true'
+      '.user-image[style*="/images/home/"], .user-image[style*="/images/user/"]',
+      re,
+      patch
+    );
+
+    hoverZoom.urlReplace(
+      res,
+      'div[style*="/images/home/"], div[style*="/images/user/"]',
+      re,
+      patch
     );
 
     callback($(res));
