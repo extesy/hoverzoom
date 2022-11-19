259c8d67363407f50d0379955cc2fd5d6d9343f7
GrosPoulet <grosnaingrincheux@gmail.com>
2022-11-19 16:46:12 +0100

    Fix for plug-in: ArtUK.org

--- plugins/artuk.js
+++ plugins/artuk.js
@@ -1,16 +1,28 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Art UK',
+    version:'2.0',
+    favicon:'artuk.png',
     prepareImgLinks:function (callback) {
+
         var res = [];
+
         $('div.credit').each(function () {
             $(this).css("pointer-events", "none");
         });
-         hoverZoom.urlReplace(res,
-            'img[src*="static.artuk.org"]',
-            /w\d+(h\d+)?/,
-            'w800h800'
+
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /w\d+(?:h\d+)?(.*)/,
+            'w800h800$1?'
         );
-        callback($(res));
+
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            /w\d+(?:h\d+)?(.*)/,
+            'w1200h1200$1?'
+        );
+
+        callback($(res), this.name);
     }
 });
