6b70917fb73342c93733c5b90db8178f2682d80a
GrosPoulet <grosnaingrincheux@gmail.com>
2022-11-26 16:19:34 +0100

    Fix for plug-in: Badoo.com

--- plugins/badoo.js
+++ plugins/badoo.js
@@ -1,7 +1,7 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Badoo',
-    version:'0.2',
+    version:'0.3',
     favicon:'badoo.svg',
     prepareImgLinks:function (callback) {
         var res = [];
@@ -18,6 +18,18 @@ hoverZoomPlugins.push({
             '',
             ['.user-card__content', '.photo-list__item', 'div']
         );
+
+        // sample: https://am1.badoo.com/profile/0zAhMACjE4MjYzNjA1OTEAIIj1AHOJhDHYEQMU_ex_NRWG8crfKc7KzOUTx9qqIIQM?from=spotlight
+        $('a[href*="/profile/"]').one('mouseenter', function() {
+            hoverZoom.prepareFromDocument($(this), this.href, function (doc) {
+                let link = $(this);
+                let data = link.data();
+                var ogImg = doc.head.querySelector('meta[property="og:image"]');
+                if (ogImg) {
+                    return ogImg.content;
+                }
+            });
+        });
 
         callback($(res), this.name);
     }
