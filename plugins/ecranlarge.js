24170c92d09a336d11bd3df7bc86e76690d32535
GrosPoulet <grosnaingrincheux@gmail.com>
2023-02-19 11:32:03 +0100

    New plug-in for: ecranlarge.com

---
+++ plugins/ecranlarge.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'ecranlarge',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var res = [];
+
+        //   sample: https://www.ecranlarge.com/media/cache/637x637/uploads/image/001/462/la-compagnie-rouge-photo-1462200.jpg
+        // fullsize: https://www.ecranlarge.com/uploads/image/001/462/la-compagnie-rouge-photo-1462200.jpg
+        hoverZoom.urlReplace(res,
+            'img[src*="ecranlarge"], [style*="url"]',
+            /\/media\/cache\/.*?\//,
+            '/'
+        );
+
+        callback($(res), this.name);
+    }
+});
