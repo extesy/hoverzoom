a92fe59c93a3bdd358d9a6fe2123783ac5df33a3
GrosPoulet <grosnaingrincheux@gmail.com>
2023-03-03 15:37:56 +0100

    New plug-in for: head-fi.org

---
+++ plugins/head-fi.js
@@ -0,0 +1,27 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'head-fi',
+    version:'1.0',
+    prepareImgLinks:function (callback) {
+
+        var res = [];
+
+        // thumbnail: https://cdn.head-fi.org/a/11981118_thumb.jpg?1675889657
+        //  fullsize: https://cdn.head-fi.org/a/11981118.jpg?1675889657
+        hoverZoom.urlReplace(res,
+            'img[src*="_thumb"], [style*="url"]',
+            '_thumb.',
+            '.'
+        );
+
+        // thumbnail: https://cdn.head-fi.org/avatars/s/141/141452.jpg?1649188084
+        //  fullsize: https://cdn.head-fi.org/avatars/o/141/141452.jpg?1649188084
+        hoverZoom.urlReplace(res,
+            'img[src*="/avatars/"]',
+            /\/avatars\/[sml]\//,
+            '/avatars/o/'
+        );
+
+        callback($(res), this.name);
+    }
+});
