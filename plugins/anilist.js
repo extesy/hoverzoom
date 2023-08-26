d6a93e358c4e2635954b0c7892cb0089678ffc1c
Oleg Anashkin <oleg.anashkin@gmail.com>
2023-08-26 11:59:22 -0700

    Add support for anilist, fixes #1223

---
+++ plugins/anilist.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'anilist',
+    version:'1.0',
+    prepareImgLinks:function (callback) {
+        var res = [];
+
+        // sample: https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx159831-TxAC0ujoLTK6.png
+        $('img[src*="/anilistcdn/media/"]').each(function() {
+            const img = $(this), src = img.attr('src');
+            img.data().hoverZoomSrc = [src];
+            img.addClass('hoverZoomLink');
+            res.push(img);
+        });
+
+        callback($(res), this.name);
+    }
+});
