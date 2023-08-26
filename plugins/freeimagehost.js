283f66028999d556dff75796ee26b6886c5d4142
Oleg Anashkin <oleg.anashkin@gmail.com>
2023-08-26 12:06:02 -0700

    Add support for freeimagehost, fixes #1222

---
+++ plugins/freeimagehost.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'Freeimage Host',
+    version:'1.0',
+    prepareImgLinks:function (callback) {
+        var res = [];
+
+        // thumbnail: https://iili.io/HyzgrgI.md.jpg
+        // fullsize:  https://iili.io/HyzgrgI.jpg
+        hoverZoom.urlReplace(res,
+            'img[src]',
+            '.md.',
+            '.'
+        );
+
+        callback($(res), this.name);
+    }
+});
