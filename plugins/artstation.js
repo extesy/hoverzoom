87b88f9f1c83f8cf272e7e40fe1d8ca4952e6791
GrosPoulet <grosnaingrincheux@gmail.com>
2023-10-20 11:36:13 +0200

    Fix for ArtStation plug-in (#1241)

--- plugins/artstation.js
+++ plugins/artstation.js
@@ -1,16 +1,22 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'artstation.com',
-    version:'0.1',
+    version:'0.2',
 
     prepareImgLinks: function (callback) {
         const res = [];
-
         hoverZoom.urlReplace(res,
-            'img[src*="artstation"]',
+            'img[src]',
+            /(\/[^0-9]*\/\d+\/\d+\/\d+\/)(\d+\/)?(small|smaller_square|small_square|micro_square|medium|medium_wide|default)\//,
+            '$1original/',
+            'a'
+        );
+        
+        hoverZoom.urlReplace(res,
+            'img[src]',
             /(\/[^0-9]*\/\d+\/\d+\/\d+\/)(\d+\/)?(small|smaller_square|small_square|micro_square|medium|medium_wide|default)\//,
             '$1large/',
-            'a.project-image'
+            'a'
         );
 
         callback($(res), this.name);
