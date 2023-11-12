2350c9333e0ff5eeea38fc6ed703092d70e4b921
GrosPoulet <grosnaingrincheux@gmail.com>
2023-11-12 15:59:43 +0100

    Fix for plug-in: Rakuten

--- plugins/rakuten.js
+++ plugins/rakuten.js
@@ -1,14 +1,24 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Rakuten',
-    version:'0.1',
+    version:'0.2',
     prepareImgLinks:function (callback) {
         var res = [];
+
         hoverZoom.urlReplace(res,
             'img[src*="?_ex="]',
             /\?_ex=.*$/,
             ''
         );
+
+        // thumbnail: https://fr.shopping.rakuten.com/photo/2391265187_L_NOPAD.jpg
+        //  fullsize: https://fr.shopping.rakuten.com/photo/2391265187.jpg
+        hoverZoom.urlReplace(res,
+            'img[src*="_"]',
+            /_(XS|S|M|MS|ML|L).*?\./,
+            '.'
+        );
+
         callback($(res), this.name);
     }
 });
