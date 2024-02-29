86f39803b293952de814e11edf12a3dbd0e9ca17
reoh <jazzysenpai@gmail.com>
2024-02-29 18:29:26 +0000

    Update mediawiki thumbnail selector
    Keep specificity to mediawiki

--- plugins/mediawiki_a.js
+++ plugins/mediawiki_a.js
@@ -8,7 +8,7 @@ hoverZoomPlugins.push({
 
         // thumbnail: /images/thumb/2/26/Senntisten_Kree%27arra_vs_Nodon.png/534px-Senntisten_Kree%27arra_vs_Nodon.png?de6e2
         //  fullsize: https://runescape.wiki/images/2/26/Senntisten_Kree%27arra_vs_Nodon.png
-        $('img[src*="thumb/"], image[href*="wiki"]').each(function() {
+        $('.mediawiki img[src*="thumb/"], image[href*="wiki"]').each(function() {
             let _this = $(this);
             let src = '';
             let srcs = [];
