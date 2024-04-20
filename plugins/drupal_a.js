76208f6e9f3c0eb1727fd5a5a70b658d125fa0cf
GrosPoulet <grosnaingrincheux@gmail.com>
2024-04-20 20:00:38 +0200

    Fix for plug-in: drupal_a

--- plugins/drupal_a.js
+++ plugins/drupal_a.js
@@ -1,7 +1,7 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'drupal_a',
-    version:'0.2',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
 
@@ -15,23 +15,16 @@ hoverZoomPlugins.push({
         //      -> https://archeologie.culture.gouv.fr/sites/archeologie/files/2021-09/img_2304_0.jpg?h=1017c59c&itok=NRsFLnj1
         // sample: https://totalenergies.com/sites/g/files/nytnzq121/files/styles/350x230/public/images/2023-04/TotalEnergies_Seagreen_eolien_en_mer_0.jpg?itok=S7pL9WsE
         //      -> https://totalenergies.com/sites/g/files/nytnzq121/files/images/2023-04/TotalEnergies_Seagreen_eolien_en_mer_0.jpg?itok=S7pL9WsE
+        // sample: https://assets.meretmarine.com/s3fs-public/styles/thumbnail_extra_lg/public/images/2024-04/Optimarin_Service_RED_lr-1024x684.jpg?h=199d8c1f&itok=H0ZWMf5_
+        //      -> https://assets.meretmarine.com/s3fs-public/images/2024-04/Optimarin_Service_RED_lr-1024x684.jpg?h=199d8c1f&itok=H0ZWMf5_
 
-        const rePublic = /(^.*\/sites\/.*\/files)\/.*\/public\/(.*)/;
-        const rePrivate = /(^.*\/system\/files)\/.*\/private\/(.*)/;
-        const reReplace = '$1/$2';
+        const reReplace = '$1/$3';
+        const reFind = /(^.*)\/[^\/]{0,}styles[^\/]{0,}\/.*\/(public|private)\/(.*)/;
 
         function findFullsizeUrl(link, src) {
-            let fullsizeUrl = src.replace(rePublic, reReplace);
-            if (fullsizeUrl == src) {
-                fullsizeUrl = src.replace(rePrivate, reReplace);
-                if (fullsizeUrl == src) return;
-            }
 
-            // decode ASCII characters, for instance: '%2C' -> ','
-            // NB: this operation must be try/catched because url might not be well-formed
-            try {
-                fullsizeUrl = decodeURIComponent(fullsizeUrl);
-            } catch {}
+            let fullsizeUrl = src.replace(reFind, reReplace);
+            if (fullsizeUrl == src) return;
 
             if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
             if (link.data().hoverZoomSrc.indexOf(fullsizeUrl) == -1) {
@@ -40,11 +33,11 @@ hoverZoomPlugins.push({
             }
         }
 
-        $('img[src*="/files/"]').filter(function() { return /\/sites\/.*\/files\//.test(this.src) || /\/system\/files\//.test(this.src) }).each(function() {
+        $('img[src*="/public/"], img[src*="/private/"]').each(function() {
             findFullsizeUrl($(this), this.src);
         });
 
-        $('[style*=url]').filter(function() { return /\/sites\/\.*\/files\//.test(this.style.backgroundImage) || /\/system\/files\//.test(this.style.backgroundImage) }).each(function() {
+        $('[style*=url]').filter(function() { return /\/(public|private)\//.test(this.style.backgroundImage) }).each(function() {
             // extract url from style
             var backgroundImage = this.style.backgroundImage;
             const reUrl = /.*url\s*\(\s*(.*)\s*\).*/i
