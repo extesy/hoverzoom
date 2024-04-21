48965781c9b765fefb0b126af4814170d585dba1
GrosPoulet <grosnaingrincheux@gmail.com>
2024-04-21 18:52:25 +0200

    New plug-in for: kotnauction.com (#1332)

---
+++ plugins/kotnauction.js
@@ -0,0 +1,18 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'kotnauction',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var name = this.name;
+        var res = [];
+
+        $('img[src]').each(function () {
+            var link = $(this);
+            const src = this.src;
+            link.data().hoverZoomSrc = [src + '?123456'];
+            res.push(link);
+        });
+
+        callback($(res), this.name);
+    }
+});
