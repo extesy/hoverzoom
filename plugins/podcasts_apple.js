5cee54a23ca58f2c0b15ea1856f75d1f552a1ab6
GrosPoulet <grosnaingrincheux@gmail.com>
2024-05-04 11:19:14 +0200

    New plug-in for: Apple Podcasts
    - Podcasts are played when hovering the "Play" button
    - HZ+ player allows user to save audio files (.mp3)

---
+++ plugins/podcasts_apple.js
@@ -0,0 +1,31 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push( {
+    name: 'podcasts_apple',
+    version: '1.0',
+    prepareImgLinks: function(callback) {
+        var name = this.name;
+
+        $('button[id*="audio-controls-playback"]').on('mouseover', function() {
+            const link = $(this);
+            let data = link.data();
+
+            if (data.hoverZoomMouseOver) return;
+            data.hoverZoomMouseOver = true;
+
+            if ($('audio')[0] == undefined) return;
+            const url = $('audio')[0].src;
+
+            data.hoverZoomSrc = [url];
+            callback(link, name);
+
+            // Podcast is played iff the cursor is still over the player
+            if (link.data().hoverZoomMouseOver)
+                hoverZoom.displayPicFromElement(link, true);
+
+        }).on('mouseleave', function () {
+            const link = $(this);
+            link.data().hoverZoomMouseOver = false;
+        });
+
+    }
+});
