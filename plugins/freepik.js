e0021e42c55fd483e8b19b16465cc949762d4aa0
GrosPoulet <grosnaingrincheux@gmail.com>
2024-05-11 11:27:23 +0200

    Fix for plug-in: Freepik.com
    added support for videos

--- plugins/freepik.js
+++ plugins/freepik.js
@@ -1,26 +1,42 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'Freepik',
-    version:'0.2',
+    version:'0.3',
     prepareImgLinks:function (callback) {
         var res = [];
+
         $('a img[src*="/th/"]').each(function() {
             var img = this,
-                link = $(this.parentNode),
-                url = link.attr('href'),
+                link = $(this).parents('a')[0],
                 imgNum = img.src.match(/th\/(.*)\./)[1];
+            link = $(link);
+            var url = link.prop('href');
             url = url.replace(/_\d+\.htm/, '_' + imgNum + '.jpg').replace('//www.', '//static.');
             link.data().hoverZoomSrc = [url];
             res.push(link);
         });
-        $('a img[src*="\?size"]').each(function() {
+
+        $('a img[src]:not(.avatar-img), img[src]:not(.avatar-img)').each(function() {
             var img = this,
-                link = $(this.parentNode),
+                link = $(this).parents('a')[0],
                 url = img.src;
-            url = url.replace(/\?size.*/, '');
+            link = link == undefined ? $(this) : $(link);
+            url = url.replace(/\.(jpe?g|png|svg).*/, '.$1?w=5000');
+            if (url.indexOf('.png') != -1) url = url.replace(/\/(128|256)\// , '/512/'); // for icons only
             link.data().hoverZoomSrc = [url];
             res.push(link);
         });
+
+        // videos
+        $('a video source[data-src]').each(function() {
+            var source = this,
+                link = $(this).parents('a')[0],
+                url = source.dataset.src;
+            link = $(link);
+            link.data().hoverZoomSrc = [url];
+            res.push(link);
+        });
+
         callback($(res), this.name);
     }
 });
