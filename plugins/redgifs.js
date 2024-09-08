eaf04c5abec05ec307caafe10266e33060cf9bff
fasolias <replaygate7@hotmail.com>
2024-09-08 18:19:33 +0300

    Update redgifs.js

--- plugins/redgifs.js
+++ plugins/redgifs.js
@@ -1,21 +1,19 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
+    name:'redgifs.com',
+    version:'0.4',
+    prepareImgLinks:function (callback) {
     name: 'redgifs.com',
     version: '0.4',
     prepareImgLinks: function(callback) {
         var self = this;
         var res = [];
         var name = this.name;
-        var apiUrl = 'https://api.redgifs.com/v2';
-
-        if (!self.redgifsToken) {
-            $.get(`${apiUrl}/auth/temporary`, function(data) {
-                if (data && data.token) {
-                    self.redgifsToken = data.token;
-                }
+@@ -16,7 +16,21 @@ hoverZoomPlugins.push({
             });
         }
 
+        $('a[href*="redgifs.com/"]').one('mouseenter', function () {
         if (options.allowHeadersRewrite) {
             chrome.runtime.sendMessage({
                 action: "storeHeaderSettings",
@@ -34,51 +32,11 @@ hoverZoomPlugins.push({
             const link = $(this);
             const gfyId = this.href.replace(/.*redgifs.com\/(..\/)?(\w+\/)?(\w+)(?:\.\w+)?/, '$3');
 
-            chrome.runtime.sendMessage({
-                action: 'ajaxGet',
-                url: `${apiUrl}/gifs/${gfyId}`,
-                headers: [{
-                    header: 'Authorization',
-                    value: `Bearer ${self.redgifsToken}`,
-                }],
-            }, (response) => {
-                let data;
-
-                try {
-                    data = JSON.parse(response);
-                } catch (e) {
-                    return;
-                }
-
-                if (data && data.gif) {
-                    link.data().hoverZoomSrc = [options.zoomVideos ? data.gif.urls.hd : data.gif.urls.gif];
-                    callback(link, name);
-                    hoverZoom.displayPicFromElement(link);
-                }
-            });
-        });
-
-        // sample:   https://thumbs2.redgifs.com/StickySereneMoray-small.jpg
-        // fullsize: https://thumbs2.redgifs.com/StickySereneMoray-large.jpg
-        hoverZoom.urlReplace(res,
-            'img[src]',
-            /-(small|medium)/,
-            '-large'
-        );
-
-        // mobile version:   https://thumbs2.redgifs.com/CookedUprightAfricanparadiseflycatcher-mobile.mp4
-        // best quality version: https://thumbs2.redgifs.com/CookedUprightAfricanparadiseflycatcher.mp4
-        $('video:not(.hoverZoomMouseover)').addClass('hoverZoomMouseover').one('mouseover', function() {
-            let link = $(this);
-            let src = this.src;
-            if (src == "") {
-                let srcs = $(this).find('source[src]');
-                if (srcs.length != 1) return;
-                src = srcs[0].src;
-            }
+@@ -65,12 +79,16 @@ hoverZoomPlugins.push({
             let videoUrlMobile = src;
             let videoUrlBestQuality = videoUrlMobile.replace('-mobile', '');
             let videoUrl = videoUrlMobile + '.video';
+            if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
             if (link.data().hoverZoomSrc == undefined) {
                 link.data().hoverZoomSrc = []
             }
@@ -86,6 +44,7 @@ hoverZoomPlugins.push({
                 link.data().hoverZoomSrc.unshift(videoUrl);
             }
             videoUrl = videoUrlBestQuality + '.video';
+            if (link.data().hoverZoomSrc == undefined) { link.data().hoverZoomSrc = [] }
             if (link.data().hoverZoomSrc == undefined) {
                 link.data().hoverZoomSrc = []
             }
@@ -95,7 +54,6 @@ hoverZoomPlugins.push({
             callback(link, name);
             hoverZoom.displayPicFromElement(link);
         });
-
         if (res.length) {
             callback($(res), name);
         }
