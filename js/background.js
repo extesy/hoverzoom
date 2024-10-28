697928607562d2e789cd965a52a09e754de1ac44
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-10-28 18:59:36 -0500

    Change webRequest Options type to match webRequest event

--- js/background.js
+++ js/background.js
@@ -379,14 +379,14 @@ function addWebRequestListeners() {
         chrome.webRequest.onBeforeSendHeaders.addListener(updateRequestHeaders, { urls : ["<all_urls>"] }, [
             'blocking',
             'requestHeaders',
-            chrome.webRequest.OnSendHeadersOptions.EXTRA_HEADERS,
+            chrome.webRequest.onBeforeSendHeadersOptions.EXTRA_HEADERS,
         ].filter(Boolean));
     }
     if (!chrome.webRequest.onHeadersReceived.hasListener(updateResponseHeaders)){
         chrome.webRequest.onHeadersReceived.addListener(updateResponseHeaders, { urls : ["<all_urls>"] }, [
             'blocking',
             'responseHeaders',
-            chrome.webRequest.OnSendHeadersOptions.EXTRA_HEADERS,
+            chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS,
         ].filter(Boolean));
     }
     
