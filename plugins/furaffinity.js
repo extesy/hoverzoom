e8cc1ca575d2bbaeceb8ab666e16d687fd5f0175
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-10-28 21:31:42 -0500

    Updated variable scopes

--- plugins/furaffinity.js
+++ plugins/furaffinity.js
@@ -3,12 +3,12 @@ hoverZoomPlugins.push({
     name:'FurAffinity',
     version:'0.2',
     prepareImgLinks:function (callback) {
-        var res = [];
+        let res = [];
         $('a[href*="/view/"]:not(.hoverZoomMouseover)').filter(function() {
 			return this.href.match(/\/view\/\d+\/$/);
 		}).addClass('hoverZoomMouseover').one('mouseover', function() {
             hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
-                var img = doc.querySelector('img[data-fullview-src]');
+                const img = doc.querySelector('img[data-fullview-src]');
                 const fullImg = img ? 'http:' + img.dataset.fullviewSrc : null;
                 return fullImg
             });
