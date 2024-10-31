8fdf207adc7be76f77d32b932276e5b1c217a6be
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-10-31 15:01:13 -0500

    Improve load times

--- plugins/gifbin.js
+++ plugins/gifbin.js
@@ -4,14 +4,15 @@ hoverZoomPlugins.push({
     prepareImgLinks:function (callback) {
         let res = [];
         
-        //TODO: Fix issue causing repeated warning: "Invalid URI. Load of media resource  failed."
-        $('img[src*="/tn_"]').each(function () { 
-            let img = $(this);
-            let src = img.attr('src');
-
-            src = src.replace('tn_', '').replace(/gif$/, 'webm')
-            img.data().hoverZoomSrc = ['https://gifbin.com' + src];
-            res.push(img);
+        //TODO: Fix status code 206 and repeated warning: "Invalid URI. Load of media resource  failed."
+        $('a[title][href*="/"]:not([class][poster])').filter(function() {
+			return this.href.match(/gifbin\.com\/\d+$/);
+		}).one('mouseover', function () { 
+            hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
+                let img = doc.getElementById('gif');
+                img = img.innerHTML.match(/source src="(.+\.mp4)"/);
+                return img ? img[1] : false;
+            });
         });
 
         callback($(res), this.name);
