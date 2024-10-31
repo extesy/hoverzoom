ce3af02ad9ef8d7208cbc08a30aeda6d345a9314
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-10-30 23:32:31 -0500

    Fix for plug-in: Gifbin

--- plugins/gifbin.js
+++ plugins/gifbin.js
@@ -2,11 +2,18 @@
 hoverZoomPlugins.push({
     name:'gifbin',
     prepareImgLinks:function (callback) {
-        $('a[href*="gifbin.com/"]').one('mouseover', function() {
-            hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
-                var img = doc.getElementById('gif');
-                return img ? img.src : false;
-            });
+        let res = [];
+        
+        //TODO: Fix issue causing repeated warning: "Invalid URI. Load of media resource  failed."
+        $('img[src*="/tn_"]').each(function () { 
+            let img = $(this);
+            let src = img.attr('src');
+
+            src = src.replace('tn_', '').replace(/gif$/, 'webm')
+            img.data().hoverZoomSrc = ['https://gifbin.com' + src];
+            res.push(img);
         });
+
+        callback($(res), this.name);
     }
 });
