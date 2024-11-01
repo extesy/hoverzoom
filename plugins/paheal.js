adb7a5faf559ad805659f93fc899b0e900a324af
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-11-01 13:53:07 -0500

    Plugin fix: Rule34 videos not working

--- plugins/paheal.js
+++ plugins/paheal.js
@@ -3,12 +3,17 @@ hoverZoomPlugins.push({
     name:'Paheal.net',
     version:'0.1',
     prepareImgLinks:function (callback) {
-        var res = [];
-        hoverZoom.urlReplace(res,
-            'img[src*="thumbs"]',
-            ['thumbs', 'thumbs'],
-            ['images', 'images']
-        );
+        let res = [];
+
+        $('img[src*="thumbs"]').each(function() {
+            let img = $(this);
+            let src = 'https://rule34.paheal.net' + img.attr('src');
+
+            src = src.replace('thumbs', 'images').replace('thumb', 'image').replace(/jpg$/, '');
+            img.data().hoverZoomSrc = [src + 'jpg', src + 'mp4'];
+            res.push(img);
+        });
+
         callback($(res), this.name);
     }
 });
