ec084a3630e9a97ee1b6fb0abdc13ef4c1ee92cc
Candy Doe <30478020+LiliaDoe@users.noreply.github.com>
2024-11-01 19:30:56 -0500

    New-plug-in-for-Etejo (NOT WORKING)
    Not sure why the popup isn't showing

---
+++ plugins/etejo.js
@@ -0,0 +1,20 @@
+﻿var hoverZoomPlugins = hoverZoomPlugins || [];
+hoverZoomPlugins.push({
+    name:'Etejo',
+    version:'0.1',
+    prepareImgLinks:function (callback) {
+        let res = [];
+
+        // This should work, yet no image shows on hover
+        // I suspect Etejo has something preventing it
+        $('img[src*="Users/"]').one('mouseover', function () {
+            let img = $(this);
+            let src = img.attr('src');
+
+            img.data().hoverZoomSrc = ['https://etejo.com/' + src];
+            res.push(img);
+        });
+
+        callback($(res), this.name);
+    }
+});
