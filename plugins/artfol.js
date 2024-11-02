b978e229b27915dda038100aac2e0ff3cff8f6b5
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-02 15:38:05 -0500

    Fixed name and version

--- plugins/artfol.js
+++ plugins/artfol.js
@@ -1,10 +1,11 @@
 var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
-    name:'artstation.com',
-    version:'0.2',
+    name:'artfol.co',
+    version:'0.1',
 
     prepareImgLinks: function (callback) {
         const res = [];
+        
         $('a[href*="a/"][title]:not(.hoverZoomMouseover)').each(function() {
             let img = $(this);
             let src = img[0].innerHTML.match(/\/medium(.*\.jpg)/)[1];
