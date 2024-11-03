192749e16f2bfefc7d11d703c5b8506d154c94c8
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-02 21:51:18 -0500

    Remove extra whitespace

--- js/options.js
+++ js/options.js
@@ -152,7 +152,7 @@ function saveOptions(exportSettings = false) {
     if (exportSettings) { return JSON.stringify(options) }
     localStorage.options = JSON.stringify(options);
     sendOptions(options);
-    restoreOptions();  
+    restoreOptions();
 
     return false;
 }
