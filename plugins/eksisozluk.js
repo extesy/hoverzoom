27c4975a927e09bafeac945116e22edf4f480bd7
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-06 19:16:55 -0600

    Update variable scopes

--- plugins/eksisozluk.js
+++ plugins/eksisozluk.js
@@ -3,15 +3,15 @@ hoverZoomPlugins.push( {
     name: 'eksisozluk',
     version: '1.0',
     prepareImgLinks: function(callback) {
-        var res = [];
-
+        const res = [];
+        
         // page hosting link to img: https://eksisozluk.com/entry/140581590
         // link to img:              https://soz.lk/i/3jadcz74
         // fullsize img:             https://cdn.eksisozluk.com/2022/7/28/3/3jadcz74.jpg
         $('a[href*="/soz.lk/"]:not(.hoverZoomMouseover)').addClass('hoverZoomMouseover').one('mouseover', function() {
 
-            var link = undefined;
-            var href = undefined;
+            let link = undefined;
+            let href = undefined;
 
             href = this.href;
             link = $(this);
@@ -19,10 +19,8 @@ hoverZoomPlugins.push( {
             // clean previous result
             link.data().hoverZoomSrc = [];
             hoverZoom.prepareFromDocument($(this), this.href, function(doc) {
-
                 let img = doc.querySelector('img');
                 if (img) return img.src;
-
             }, false); // get source sync
         });
 
