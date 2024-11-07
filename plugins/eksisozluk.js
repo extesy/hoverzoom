4e5b9edf59f8893e2774c2e3519d4b5fff5d66ac
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-07 16:29:46 -0600

    Plugin fix: Eksisozluk
    It extracts the date of the post and creates an image link with it. Might need to be expanded. This site needs a referrer with its ajax request, and this is a workaround

--- plugins/eksisozluk.js
+++ plugins/eksisozluk.js
@@ -8,15 +8,16 @@ hoverZoomPlugins.push({
         // page hosting link to img: https://eksisozluk.com/entry/140581590
         // link to img:              https://soz.lk/i/3jadcz74
         // fullsize img:             https://cdn.eksisozluk.com/2022/7/28/3/3jadcz74.jpg
-        $('a[href*="/soz.lk/"]:not(.hoverZoomMouseover)').addClass('hoverZoomMouseover').one('mouseover', function () {
+        $('a[href*="/soz.lk/"]:not(.hoverZoomLink)').one('mouseover', function () {
             const link = $(this);
-
             // clean previous result
             link.data().hoverZoomSrc = [];
-            hoverZoom.prepareFromDocument($(this), this.href, (doc) => {
-                const img = doc.querySelector('img');
-                return img ? img.src : null;
-            }); // get source sync
+            // extract date from post and create image link
+            const date = link.parent().next().find('.entry-date.permalink')[0].innerText.match(/0?(\d+)\.0?(\d+)\.(\d+)/);
+            const src = this.href.replace(/soz\.lk\/i\/(.{1})/, 'cdn.eksisozluk.com/' + date[3] + '/' + date[2] + '/' + date[1] + '/$1/$1');
+            link.data('hoverZoomSrc', [src + '.png', src + '.jpg']);
+
+            link.addClass('hoverZoomLink')
         });
 
         callback($(res), this.name);
