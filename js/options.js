9be43363766b433aaae873057fabb7ce739bf82a
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-22 20:59:03 -0600

    Add handling if tap and hold are both for the same button

--- js/options.js
+++ js/options.js
@@ -131,10 +131,16 @@ function saveOptions(exportSettings = false) {
     actionKeys.forEach(function(key) {
         var id = key[0].toUpperCase() + key.substr(1);
         options[key] = parseInt($('#sel' + id).val());
-        if (options[key] == -3)
+        if (options[key] == -3){
             options.rightMouseActionKey == -3;
-        if (options[key] == -4)
+            if (options[key] == -1) // if both selected
+                options.rightMouseActionKey == -5;
+        }
+        if (options[key] == -4) {
             options.middleMouseActionKey == -4;
+            if (options[key] == -2) // if both selected
+                options.middleMouseActionKey == -6;
+        }
     });
 
     options.showDetailFilename = $('#chkShowDetailFilename')[0].checked;
