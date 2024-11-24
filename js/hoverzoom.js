ff4a3921c5dd459b74fb0e0a5c0e83197e6ca87e
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2024-11-23 22:33:16 -0600

    Make full zoom key fill window more

--- js/hoverzoom.js
+++ js/hoverzoom.js
@@ -503,6 +503,7 @@ var hoverZoom = {
                 } else if (fullZoomKey) {
                     // naturalWidth replaced with wndWidth to make image fill window
                     imgFullSize.width(Math.min(wndWidth, wndWidth - padding - 2 * scrollBarWidth)); 
+                    statusBarHeight = 0;
                 } else if (fullZoom) {
                     imgFullSize.width(Math.min(srcDetails.naturalWidth * zoomFactor, wndWidth - padding - 2 * scrollBarWidth));
                 } else if (displayOnRight) {
