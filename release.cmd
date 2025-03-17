222b5ef447544ba737fae1e7b944b0290ed7894b
Oleg Anashkin <oleg.anashkin@gmail.com>
2025-03-17 09:57:05 -0700

    add rules.json to release script

--- release.cmd
+++ release.cmd
@@ -1,4 +1,4 @@
 @echo off
 del hoverzoom.zip >nul
-winrar a -afzip -m5 -r hoverzoom.zip _locales css fonts html images js plugins LICENSE manifest.json -xnode_modules -x.idea
+winrar a -afzip -m5 -r hoverzoom.zip _locales css fonts html images js plugins LICENSE manifest.json ruleset.json -xnode_modules -x.idea
 
