c2c265e3fbd1580cef6d72b37cca31fddb232e78
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2025-03-26 15:16:15 -0500

    create separate function to createBlobUrl (not sure if comment is accurate enough)

--- js/background.js
+++ js/background.js
@@ -51,7 +51,7 @@ async function ajaxRequest(request, sendResponse) {
                     case 'DOWNLOAD':
                         const arrayBuffer = await fetchResponse.arrayBuffer();
                         const contentType = fetchResponse.headers.get('content-type') || 'application/octet-stream';
-                        const blobBin = new Blob([arrayBuffer], { type: contentType });
+                        const blobBin = new Blob([arrayBuffer], { type: contentType }); 
                         const blobUrl = await createBlobUrl(blobBin);
                         downloadFile(blobUrl, filename, conflictAction, sendResponse);
                         break;
@@ -73,9 +73,11 @@ async function ajaxRequest(request, sendResponse) {
 }
 
 // For ajax-based image loading, Firefox needs an Object URL, Chrome needs a Data URI
-async function createBlobUrl(blobBin) {
+function createBlobUrl(blobBin) {
     const isChromiumBased = !!navigator.userAgentData?.brands?.some(item => item.brand === 'Chromium');
-    return isChromiumBased ? await blobToDataURI(blobBin) : URL.createObjectURL(blobBin);
+    const blobDataURI = blobToDataURI(blobBin);
+    const blobObjectURL = URL.createObjectURL(blobBin);
+    return isChromiumBased ? blobDataURI : blobObjectURL;
 }
 
 function blobToDataURI(blob) {
