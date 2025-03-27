7d42508c54d57542e0b1ea75ba6a21944de6cdab
Lilia <30478020+LiliaDoe@users.noreply.github.com>
2025-03-27 12:21:37 -0500

    Remove unnecessary function

--- js/background.js
+++ js/background.js
@@ -51,8 +51,10 @@ async function ajaxRequest(request, sendResponse) {
                     case 'DOWNLOAD':
                         const arrayBuffer = await fetchResponse.arrayBuffer();
                         const contentType = fetchResponse.headers.get('content-type') || 'application/octet-stream';
-                        const blobBin = new Blob([arrayBuffer], { type: contentType });
-                        const blobUrl = await createBlobUrl(blobBin);
+                        const blobBin = new Blob([arrayBuffer], { type: contentType }); 
+                        
+                        // For ajax-based image loading, Firefox needs an Object URL, Chrome needs a Data URI
+                        const blobUrl = isChromiumBased ? await blobToDataURI(blobBin) : URL.createObjectURL(blobBin);
                         downloadFile(blobUrl, filename, conflictAction, sendResponse);
                         break;
                     case 'URL':
@@ -72,10 +74,7 @@ async function ajaxRequest(request, sendResponse) {
     }
 }
 
-// For ajax-based image loading, Firefox needs an Object URL, Chrome needs a Data URI
 async function createBlobUrl(blobBin) {
-    const isChromiumBased = !!navigator.userAgentData?.brands?.some(item => item.brand === 'Chromium');
-    return isChromiumBased ? await blobToDataURI(blobBin) : URL.createObjectURL(blobBin);
 }
 
 function blobToDataURI(blob) {
