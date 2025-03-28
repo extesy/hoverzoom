025e6b3fd06a6f1a58582099ae2f8a0d83856bcf
Oleg Anashkin <oleg.anashkin@gmail.com>
2025-03-28 15:55:59 -0700

    Fix vk.com plugin

--- plugins/vk.js
+++ plugins/vk.js
@@ -1,35 +1,26 @@
 ﻿var hoverZoomPlugins = hoverZoomPlugins || [];
 hoverZoomPlugins.push({
     name:'VK.com',
-    version:'1.4',
+    version:'1.5',
     prepareImgLinks:function (callback) {
-
         const res = [];
 
-        $('div[data-task-click="WallPost/openPhoto"], a[data-task-click="WallPost/openPhoto"], a.page_post_thumb_wrap').each(function () {
-            let link = $(this), data = this.getAttribute('data-options');
-            if (!data)
-                data = this.getAttribute('onclick');
-            if (!data)
-                return;
-            //parse onclick function body in order to extract alternative images urls
-            const index1 = data.indexOf('{');
-            const index2 = data.lastIndexOf('}');
-            const json = data.substring(index1, index2 + 1);
-            if (json) {
-                let url = "";
-                try {
-                    const j = JSON.parse(json);
-                    const jj = j.base === undefined || j.base === "" ? j.temp : j.base;
-                    if (jj.w) url = jj.w;
-                    else if (jj.z) url = jj.z;
-                    else if (jj.y) url = jj.y;
-                    else if (jj.x) url = jj.x;
-
-                    link.data().hoverZoomSrc = [url];
-                    res.push(link);
-                } catch(e) {}
+        $('img[src*=".userapi.com/s/"], div[style*=".userapi.com/s/"]').one('mouseenter', function () {
+            let link = $(this), url = this.getAttribute('src');
+            if (!url) {
+                url = this.style.backgroundImage;
+                url = url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
             }
+            // example url: https://sun6-21.userapi.com/s/v1/ig2/g7tzQqEL8XklUJn372fl13rbTyD4wdh8d5pfQbdErfWrUZEM3L7KF3xzBCbb8QouT2VlAKHy0M7USE242_W6jXmg.jpg?quality=95&crop=213,45,716,716&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640&ava=1&u=t53O3an8Miwp5cxILV0zmDbhSSSzB5h8y-qrEvpnvPA&cs=400x400
+            // desired url: https://sun6-21.userapi.com/s/v1/ig2/g7tzQqEL8XklUJn372fl13rbTyD4wdh8d5pfQbdErfWrUZEM3L7KF3xzBCbb8QouT2VlAKHy0M7USE242_W6jXmg.jpg?quality=95&crop=213,45,716,716&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640&ava=1&u=t53O3an8Miwp5cxILV0zmDbhSSSzB5h8y-qrEvpnvPA&cs=640x640
+            const asMatch = url.match(/as=([^&]+)/);
+            if (!asMatch) return;
+            const sizes = asMatch[1].split(',');
+            const lastSize = sizes[sizes.length - 1];
+            url = url.replace(/cs=[^&]+/, 'cs=' + lastSize);
+            link.data().hoverZoomSrc = [url];
+            link.addClass('hoverZoomLink');
+            hoverZoom.displayPicFromElement(link);
         });
 
         callback($(res), this.name);
